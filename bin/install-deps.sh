6b7e87d1ba1e73158a99bbd9f67e9d26714d257f
Eenae <alexey@mixbytes.io>
2020-07-17 17:21:32 +0300

    basic dev stuff

--- bin/install-deps.sh
+++ bin/install-deps.sh
@@ -8,6 +8,4 @@ BIN_DIR="$(cd $(dirname $0) && pwd)"
 . "$BIN_DIR/.lib.sh"
 
 
-npm i @aragon/cli eth-json-rpc-filters
-
-"$ARAGON" ipfs install --local
+( cd "$ROOT_DIR" && npm i && "$ARAGON" ipfs install --local )
