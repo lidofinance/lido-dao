a1c220154cf9b426297226034b606e9c0edaf90c
KRogLA <i@krogla.me>
2020-09-10 17:32:29 +0200

    docker containers for ETH1 node and IPFS

---
+++ docker/ipfs/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+CACHE_DIR=/ipfs-cache
+mkdir -p $CACHE_DIR
+if [ ! "$(ls -A $CACHE_DIR)" ]; then
+  echo "Initializing ipfs data from snapshot"
+  tar -zxf aragen.tgz -C $CACHE_DIR --strip 2 package/ipfs-cache
+fi
+
+echo "Starting ipfs..."
+export IPFS_PROFILE=server
+/usr/local/bin/start_ipfs add -r -Q $CACHE_DIR
+/usr/local/bin/start_ipfs daemon --migrate=true
