6888c28e3c2f17800542bf0a38f76c6e6baa467a
KRogLA <i@krogla.me>
2020-09-23 12:08:52 +0200

    dkg scripts fix

--- dkg.sh
+++ dkg.sh
@@ -7,6 +7,7 @@ ROOT=$PWD
 DOCKER_DIR=$ROOT/docker/dkg-pubkey
 mkdir -p $DOCKER_DIR
 cd $DOCKER_DIR
+rm -rf dc4bc kyber-bls12381
 git clone -b fix/full-flow-test git@github.com:depools/dc4bc.git
 git clone git@github.com:depools/kyber-bls12381.git
 docker build -t dkg .
