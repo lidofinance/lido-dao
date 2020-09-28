1d4f9b734745b08a0ea394ccd948cc0336da0031
itaven <kurn_97@mail.ru>
2020-09-29 02:58:05 +0300

    eth2 helper

---
+++ test-e2e/test-helpers/eth2/requestHelper.js
@@ -0,0 +1,10 @@
+const format = require('../../../automation/utils')
+// TODO request wrapper
+function getRequest() {
+  // response status
+}
+
+function postRequest() {
+  console.info('Sending POST request to {0}'.format('asd'))
+  // response status
+}
