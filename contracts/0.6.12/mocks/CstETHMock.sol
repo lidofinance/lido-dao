24d898b68bf15f7a0785dbadd8c0dbb590f463aa
lxzrv <a.lazarev@ongrid.pro>
2020-10-01 19:08:22 +0300

    Add cstETH token basic implementation

--- contracts/0.6.12/mocks/CstETHMock.sol
+++ contracts/0.6.12/mocks/CstETHMock.sol
@@ -3,6 +3,11 @@ pragma solidity 0.6.12; // latest available for using OZ
 import "../CstETH.sol";
 
 contract CstETHMock is CstETH {
+    constructor(ERC20 _stETH)
+        public
+        CstETH(_stETH)
+    { }
+
     function mint(address recipient, uint256 amount) public {
         _mint(recipient, amount);
     }
