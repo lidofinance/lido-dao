48b2c543e71f3e019f877772dad3a5307d6c7c5d
KRogLA <i@krogla.me>
2020-10-02 10:23:40 +0200

    keep quotes in params

--- deposit.sh
+++ deposit.sh
@@ -3,7 +3,7 @@ source ./.env
 set -e +u
 set -o pipefail
 
-TMP_DIR=${PWD}/tmp-deposit-cli
+TMP_DIR=$PWD/tmp-deposit-cli
 IMG="depool-deposit-cli:latest"
 
 if [[ "$(docker images -q $IMG 2> /dev/null)" == "" ]]; then
@@ -13,9 +13,9 @@ if [[ "$(docker images -q $IMG 2> /dev/null)" == "" ]]; then
   mkdir -p $TMP_DIR
   cd $TMP_DIR
   git clone -b custom-data https://github.com/depools/eth2.0-deposit-cli .
-  docker build -t $IMG .
+  docker build -t $IMG --no-cache .
+  cd -
   rm -rf $TMP_DIR
 fi
 
-docker run -it -v $PWD/data:/data deposit-cli:latest --folder /data $@
-
+docker run -it -v $PWD/data:/data $IMG --folder /data "$@"
