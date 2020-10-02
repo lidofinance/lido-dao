d85a9eb752d8941c2f58312fc801f3e8f9056fe8
KRogLA <i@krogla.me>
2020-10-02 09:36:09 +0200

    script for genarate validator keys

---
+++ deposit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source ./.env
+set -e +u
+set -o pipefail
+
+TMP_DIR=${PWD}/tmp-deposit-cli
+IMG="depool-deposit-cli:latest"
+
+if [[ "$(docker images -q $IMG 2> /dev/null)" == "" ]]; then
+  # do something
+  echo "Building deposit-cli Docker image..."
+  rm -rf $TMP_DIR
+  mkdir -p $TMP_DIR
+  cd $TMP_DIR
+  git clone -b custom-data https://github.com/depools/eth2.0-deposit-cli .
+  docker build -t $IMG .
+  rm -rf $TMP_DIR
+fi
+
+docker run -it -v $PWD/data:/data deposit-cli:latest --folder /data $@
+
