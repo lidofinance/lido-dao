7609136d24f4b08ccb2755f915f302f9e054f5a8
Azat <ext-serikov@aobi.local>
2020-10-05 15:24:53 +0600

    initial setup

--- apps/depool/app/src/script.js
+++ apps/depool/app/src/script.js
@@ -1,6 +1,20 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import Aragon, { events } from '@aragon/api'
+import {
+  getFee,
+  getFeeDistribution,
+  getWithdrawalCredentials,
+  getBufferedEther,
+  getEther2Stat,
+  getInsuranceFund,
+  getOracle,
+  getSPs,
+  getToken,
+  getTotalControlledEther,
+  getTreasury,
+  getValidatorRegistrationContract,
+} from './api'
 
 const app = new Aragon()
 
@@ -10,12 +24,16 @@ app.store(
       ...state,
     }
 
+    console.log('reducer', event)
+
     try {
       switch (event) {
-        case 'Increment':
-          return { ...nextState, count: await getValue() }
-        case 'Decrement':
-          return { ...nextState, count: await getValue() }
+        case 'FeeSet':
+          return { ...nextState, fee: await getFee() }
+        case 'FeeDistributionSet':
+          return { ...nextState, feeDistribution: await getFeeDistribution() }
+        case 'WithdrawalCredentialsSet':
+          return { ...nextState, withdrawalCredentials: await getWithdrawalCredentials() }
         case events.SYNC_STATUS_SYNCING:
           return { ...nextState, isSyncing: true }
         case events.SYNC_STATUS_SYNCED:
@@ -39,14 +57,21 @@ app.store(
  ***********************/
 
 function initializeState() {
-  return async cachedState => {
+  return async (cachedState) => {
     return {
       ...cachedState,
-      count: await getValue(),
+      fee: await getFee(),
+      // feeDistribution: await getFeeDistribution(),
+      withdrawalCredentials: await getWithdrawalCredentials(),
+      bufferedEther: await getBufferedEther(),
+      totalControlledEther: await getTotalControlledEther(),
+      token: await getToken(),
+      // validatorRegistrationContract: await getValidatorRegistrationContract(),
+      oracle: await getOracle(),
+      // SPs: await getSPs(),
+      // treasury: await getTreasury(),
+      // insuranceFund: await getInsuranceFund(),
+      ether2Stat: await getEther2Stat(),
     }
   }
-}
-
-async function getValue() {
-  return parseInt(await app.call('value').toPromise(), 10)
 }
