1c77ba1c79187fd4479201dcd96350410755d177
KRogLA <i@krogla.me>
2020-10-05 12:56:09 +0200

    add spregistry do deploy scripts

--- template/scripts/helpers/apps.js
+++ template/scripts/helpers/apps.js
@@ -9,11 +9,13 @@ const TOKEN_MANAGER_APP_ID = '0x6b20a3010614eeebf2138ccec99f028a61c811b3b1a3343b
 const STETH_APP_ID = '0x5937d846addd00601bf692837c2cd9854dacd2c55911625da04aec9c62a61a26'
 const DEPOOLORACLE_APP_ID = '0xebe89ae11ec5a76827463bd202b0551f137fdc6dad7cd69ecdf4fe553af5f77b'
 const DEPOOL_APP_ID = '0xdf4019658a996b6bc3639baa07d25c655bf826334fc5c81bb83e501905b51cb1'
+const REGISTRY_APP_ID = '0x6ca5078df26de2bcf0976b0bfba50b6ed5dac3644879214556e2789dfc78df16'
 
 const apps = [
   { name: 'steth', tld: 'depoolspm.eth', contractName: 'StETH', appId: STETH_APP_ID },
   { name: 'depool', tld: 'depoolspm.eth', contractName: 'DePool', appId: DEPOOL_APP_ID },
   { name: 'depooloracle', tld: 'depoolspm.eth', contractName: 'DePoolOracle', appId: DEPOOLORACLE_APP_ID },
+  { name: 'staking-providers-registry', tld: 'depoolspm.eth', contractName: 'SPRegistry', appId: REGISTRY_APP_ID },
   { name: 'agent', tld: 'aragonpm.eth', contractName: 'Agent', appId: AGENT_APP_ID },
   // { name: 'vault', tld: 'aragonpm.eth', contractName: 'Vault', appId: VAULT_APP_ID },
   { name: 'voting', tld: 'aragonpm.eth', contractName: 'Voting', appId: VOTING_APP_ID },
