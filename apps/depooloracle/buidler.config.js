46436ccc77a7822e8f0b937820a5256e569060ee
Sam Kozin <sam.kozin@gmail.com>
2020-10-14 01:28:15 +0300

    centralize Buidler config

--- apps/depooloracle/buidler.config.js
+++ apps/depooloracle/buidler.config.js
@@ -1,33 +1,17 @@
 const { usePlugin } = require('@nomiclabs/buidler/config')
 const hooks = require('./scripts/buidler-hooks')
 
+const baseConfig = require('../../buidler.config.js')
+
 usePlugin('@aragon/buidler-aragon')
-usePlugin("@nomiclabs/buidler-ganache")
 
 module.exports = {
+  ...baseConfig,
   paths: {
+    ...baseConfig.paths,
     root: '../..',
-    sources: './contracts/0.4.24',
   },
-  // Default Buidler configurations. Read more about it at https://buidler.dev/config/
   defaultNetwork: 'localhost',
-  networks: {
-    localhost: {
-      url: 'http://localhost:8545',
-    },
-  },
-  solc: {
-    version: '0.4.24',
-    optimizer: {
-      enabled: true,
-      runs: 200,
-    },
-    evmVersion: 'constantinople'
-  },
-  // Etherscan plugin configuration. Learn more at https://github.com/nomiclabs/buidler/tree/master/packages/buidler-etherscan
-  etherscan: {
-    apiKey: '', // API Key for smart contract verification. Get yours at https://etherscan.io/apis
-  },
   // Aragon plugin configuration
   aragon: {
     appServePort: 3011,
