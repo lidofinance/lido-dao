9d7d4b384bbc18f7ce51b3a0654c6bf2bb62af18
itaven <kurn_97@mail.ru>
2020-10-13 17:38:50 +0300

    eth1 transaction gas update

--- test-e2e/test-helpers/eth1Helper.js
+++ test-e2e/test-helpers/eth1Helper.js
@@ -4,5 +4,5 @@ export const getAccounts = async (web3) =>
   })
 
 export async function sendTransaction(web3, receipt, sender, value) {
-  await web3.eth.sendTransaction({ to: receipt, from: sender, value: value, gas: '1000000' })
+  return web3.eth.sendTransaction({ to: receipt, from: sender, value: value, gas: '3000000' })
 }
