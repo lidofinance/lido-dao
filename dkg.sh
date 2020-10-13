50b2635fdf3c9a0f662118132c31b4a5cb951b75
itaven <kurn_97@mail.ru>
2020-10-13 17:30:25 +0300

    dc4bc update

--- dkg.sh
+++ dkg.sh
@@ -8,7 +8,7 @@ DOCKER_DIR=$ROOT/docker/dkg-pubkey
 mkdir -p $DOCKER_DIR
 cd $DOCKER_DIR
 rm -rf dc4bc kyber-bls12381
-git clone -b fix/full-flow-test git@github.com:depools/dc4bc.git
+git clone git@github.com:depools/dc4bc.git
 git clone git@github.com:depools/kyber-bls12381.git
 docker build -t dkg .
 docker run -v "$ROOT/data":/data -it dkg
