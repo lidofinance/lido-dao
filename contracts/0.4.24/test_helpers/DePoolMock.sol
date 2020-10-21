253182d99fb70766e31f192e234d7fc527fa829e
Kirill Varlamov <kirill@ongrid.pro>
2020-10-21 20:50:08 +0300

    Fix Solidity Layout issues #78 #70 #3

--- contracts/0.4.24/test_helpers/DePoolMock.sol
+++ contracts/0.4.24/test_helpers/DePoolMock.sol
@@ -5,23 +5,21 @@ import "./VaultMock.sol";
 
 
 /**
-  * @dev Only for testing purposes! DePool version with some functions exposed.
-  */
+ * @dev Only for testing purposes! DePool version with some functions exposed.
+ */
 contract DePoolMock is DePool {
+    function getTotalControlledEther() external view returns (uint256) {
+        return totalControlledEther;
+    }
 
-  uint256 private totalControlledEther;
+    function initialize(ISTETH _token) public {
+        _setToken(_token);
+        initialized();
+    }
 
-  function initialize(ISTETH _token) public {
-    _setToken(_token);
-    initialized();
-  }
+    function setTotalControlledEther(uint256 _totalControlledEther) public {
+        totalControlledEther = _totalControlledEther;
+    }
 
-  function setTotalControlledEther(uint256 _totalControlledEther) public {
-    totalControlledEther = _totalControlledEther;
-  }
-
-  function getTotalControlledEther() external view returns (uint256) {
-    return totalControlledEther;
-  }
-
+    uint256 private totalControlledEther;
 }
