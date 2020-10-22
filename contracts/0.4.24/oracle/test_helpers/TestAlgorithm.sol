6261eea727aec33514da29ec7daab8ab1574d56c
lxzrv <a.lazarev@ongrid.pro>
2020-10-22 18:15:50 +0300

    Use aragon solium config, refactor accordingly #84

--- contracts/0.4.24/oracle/test_helpers/TestAlgorithm.sol
+++ contracts/0.4.24/oracle/test_helpers/TestAlgorithm.sol
@@ -2,6 +2,7 @@ pragma solidity 0.4.24;
 
 import "../Algorithm.sol";
 
+
 contract TestAlgorithm {
     function modeTest(uint256[] data) public pure returns (bool isUnimodal, uint256 mode) {
         return Algorithm.mode(data);
