00669259a2ff51fb17bcb87a120d13ed450ac2da
lxzrv <a.lazarev@ongrid.pro>
2020-10-22 18:15:50 +0300

    Use aragon solium config, refactor accordingly #84

--- contracts/0.6.12/mocks/CstETHMock.sol
+++ contracts/0.6.12/mocks/CstETHMock.sol
@@ -1,7 +1,10 @@
+// SPDX-License-Identifier: NONE
+
 pragma solidity 0.6.12; // latest available for using OZ
 
 import "../CstETH.sol";
-import "../IStETH.sol";
+import "../interfaces/IStETH.sol";
+
 
 contract CstETHMock is CstETH {
     constructor(IStETH _stETH)
