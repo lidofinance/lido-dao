0aa6044cb1ddfe90ef09dd5823dbf5f3e6d043b7
Kirill Varlamov <kirill@ongrid.pro>
2020-10-23 12:07:31 +0300

    Add explanation why different solc used #86

--- contracts/0.6.12/CstETH.sol
+++ contracts/0.6.12/CstETH.sol
@@ -1,4 +1,5 @@
-pragma solidity 0.6.12; // latest available for using OZ
+/* See contracts/COMPILERS.md */
+pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
