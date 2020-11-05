3d038c1d229f9c64196c53518b0f98d426a4057d
lxzrv <a.lazarev@ongrid.pro>
2020-11-05 16:40:47 +0300

    sps -> operators, match case

--- test/0.4.24/lido.test.js
+++ test/0.4.24/lido.test.js
@@ -108,7 +108,7 @@ contract('Lido', ([appManager, voting, user1, user2, user3, nobody]) => {
 
   // Assert reward distribution. The values must be divided by 1e15.
   const checkRewards = async ({ treasury, insurance, operator }) => {
-    const [treasury_b, insurance_b, sps_b, a1, a2, a3, a4] = await Promise.all([
+    const [treasury_b, insurance_b, operators_b, a1, a2, a3, a4] = await Promise.all([
       token.balanceOf(treasuryAddr),
       token.balanceOf(insuranceAddr),
       token.balanceOf(operators.address),
@@ -120,7 +120,7 @@ contract('Lido', ([appManager, voting, user1, user2, user3, nobody]) => {
 
     assertBn(div15(treasury_b), treasury, 'treasury token balance check')
     assertBn(div15(insurance_b), insurance, 'insurance fund token balance check')
-    assertBn(div15(sps_b.add(a1).add(a2).add(a3).add(a4)), operator, 'node operators token balance check')
+    assertBn(div15(operators_b.add(a1).add(a2).add(a3).add(a4)), operator, 'node operators token balance check')
   }
 
   it('setFee works', async () => {
