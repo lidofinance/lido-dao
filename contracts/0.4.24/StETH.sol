d968cdb0358c7656fab2a77c1e0f3f4fba722194
Sam Kozin <sam.kozin@gmail.com>
2020-11-16 01:06:29 +0300

    remove an unused import

--- contracts/0.4.24/StETH.sol
+++ contracts/0.4.24/StETH.sol
@@ -3,13 +3,11 @@ pragma solidity 0.4.24;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
 import "@aragon/os/contracts/lib/math/SafeMath.sol";
-import {ERC20 as OZERC20} from "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 import "./interfaces/ISTETH.sol";
+import "./interfaces/ILido.sol";
 
 import "./lib/Pausable.sol";
-
-import "./interfaces/ILido.sol";
 
 
 /**
