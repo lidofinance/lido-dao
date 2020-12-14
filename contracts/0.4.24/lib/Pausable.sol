eff52fd46dd43dd4e3c17a137604fdddb846fb42
Eugene Pshenichnyy <pshenichniy.eugene@gmail.com>
2020-12-14 20:52:40 +0300

    Rename UNSTOPPED -> RUNNING

--- contracts/0.4.24/lib/Pausable.sol
+++ contracts/0.4.24/lib/Pausable.sol
@@ -13,29 +13,29 @@ contract Pausable {
     event Stopped();
     event Resumed();
 
-    bytes32 internal constant UNSTOPPED_FLAG_POSITION = keccak256("lido.Pausable.unstopped");
+    bytes32 internal constant RUNNING_FLAG_POSITION = keccak256("lido.Pausable.running");
 
     modifier whenNotStopped() {
-        require(UNSTOPPED_FLAG_POSITION.getStorageBool(), "CONTRACT_IS_STOPPED");
+        require(RUNNING_FLAG_POSITION.getStorageBool(), "CONTRACT_IS_STOPPED");
         _;
     }
 
     modifier whenStopped() {
-        require(!UNSTOPPED_FLAG_POSITION.getStorageBool());
+        require(!RUNNING_FLAG_POSITION.getStorageBool());
         _;
     }
 
     function isStopped() external view returns (bool) {
-        return !UNSTOPPED_FLAG_POSITION.getStorageBool();
+        return !RUNNING_FLAG_POSITION.getStorageBool();
     }
 
     function _stop() internal whenNotStopped {
-        UNSTOPPED_FLAG_POSITION.setStorageBool(false);
+        RUNNING_FLAG_POSITION.setStorageBool(false);
         emit Stopped();
     }
 
     function _resume() internal whenStopped {
-        UNSTOPPED_FLAG_POSITION.setStorageBool(true);
+        RUNNING_FLAG_POSITION.setStorageBool(true);
         emit Resumed();
     }
 }
