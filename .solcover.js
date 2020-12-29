a0e5a9a3bd5bb49f0913b45638b96761a0f1c703
Kirill Varlamov <kirill@ongrid.pro>
2020-12-29 16:40:36 +0300

    Restore solidity coverage for HardHat #279

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,7 @@
 module.exports = {
-  skipFiles: ['template', 'test_helpers', 'oracle/test_helpers', 'nos/test_helpers', 'mocks'],
+  skipFiles: ['0.4.24/template', '0.4.24/nos/test_helpers',
+    '0.4.24/oracle/test_helpers', '0.4.24/test_helpers',
+    '0.6.12/mocks', '0.6.11/deposit_contract.sol'],
   providerOptions: {
     default_balance_ether: 10000,
     gasPrice: '0x1',
