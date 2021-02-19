c5bd468039da9b0dc60f6e0a77ca7d395d41093b
Victor Suzdalev <victor.suzdalev@gmail.com>
2021-02-19 16:01:29 +0300

    docs: add notices tp all WstETH methods

--- contracts/0.6.12/WstETH.sol
+++ contracts/0.6.12/WstETH.sol
@@ -12,14 +12,18 @@ import "./interfaces/IStETH.sol";
  * @title StETH token wrapper with static balances.
  * @dev It's an ERC20 token that represents the account's share of the total
  * supply of stETH tokens. WstETH token's balance only changes on transfers,
- * unlike StETH that is also changed when oracles report staking rewards,
- * penalties, and slashings. It's a "power user" token that might be needed to
- * work correctly with some DeFi protocols like Uniswap v2, cross-chain bridges,
- * etc.
+ * unlike StETH that is also changed when oracles report staking rewards and
+ * penalties. It's a "power user" token for DeFi protocols which don't
+ * support rebasable tokens.
  *
- * The contract also works as a wrapper that accepts stETH tokens and mints
- * wstETH in return. User can unwrap their wstETH, burning wstETH tokens and
- * receiving stETH tokens in return.
+ * The contract is also a trustless wrapper that accepts stETH tokens and mints
+ * wstETH in return. Then the user unwraps, the contract burns user's wstETH
+ * and sends user staked stETH in return.
+ *
+ * The contract provides the staking shortcut: user can send ETH with regular
+ * transfer and get wstETH in return. The contract will send ETH to StETH submit
+ * method, staking it and wrapping the received stETH.
+ *
  */
 contract WstETH is ERC20Permit {
     IStETH public stETH;
@@ -36,14 +40,15 @@ contract WstETH is ERC20Permit {
     }
 
     /**
-     * @dev Exchanges stETH to wstETH
+     * @notice Exchanges stETH to wstETH
      * @param _stETHAmount amount of stETH to wrap in exchange for wstETH
-     *
-     * Requirements:
+     * @dev Requirements:
      *  - `_stETHAmount` must be non-zero
      *  - msg.sender must approve at least `_stETHAmount` stETH to this
      *    contract.
      *  - msg.sender must have at least `_stETHAmount` of stETH.
+     * User should first approve _stETHAmount to the WstETH contract
+     * @return Amount of wstETH user receives after wrap
      */
     function wrap(uint256 _stETHAmount) external returns (uint256) {
         require(_stETHAmount > 0, "wstETH: can't wrap zero stETH");
@@ -54,13 +59,13 @@ contract WstETH is ERC20Permit {
     }
 
     /**
-     * @dev Exchanges wstETH to stETH
+     * @notice Exchanges wstETH to stETH
      * @param _wstETHAmount amount of wstETH to uwrap in exchange for stETH
-     *
-     * Requirements:
+     * @dev Requirements:
      *  - `_wstETHAmount` must be non-zero
-     *  - msg.sender must have enough stETH.
-     *  - msg.sender must have at least `_stETHAmount` stETH.
+     *  - msg.sender must have enough wstETH.
+     *  - msg.sender must have at least `_wstETHAmount` wstETH.
+     * @return Amount os stETH user receives after unwrap
      */
     function unwrap(uint256 _wstETHAmount) external returns (uint256) {
         require(_wstETHAmount > 0, "wstETH: zero amount unwrap not allowed");
@@ -71,7 +76,7 @@ contract WstETH is ERC20Permit {
     }
 
     /**
-    * @notice Send funds to the pool and get wstETH tokens
+    * @notice Shortcut to stake ETH and auto-wrap returned stETH
     */
     receive() external payable {
         uint256 shares = stETH.submit{value: msg.value}(address(0));
@@ -79,30 +84,34 @@ contract WstETH is ERC20Permit {
     }
 
     /**
+     * @notice Get amount of wstETH for a given amount of stETH
      * @param _stETHAmount amount of stETH
-     * @return Returns amount of wstETH for a given stETH amount
+     * @return Amount of wstETH for a given stETH amount
      */
     function getWstETHByStETH(uint256 _stETHAmount) external view returns (uint256) {
         return stETH.getSharesByPooledEth(_stETHAmount);
     }
 
     /**
+     * @notice Get amount of stETH for a given amount of wstETH
      * @param _wstETHAmount amount of wstETH
-     * @return Returns amount of stETH for a given wstETH amount
+     * @return Amount of stETH for a given wstETH amount
      */
     function getStETHByWstETH(uint256 _wstETHAmount) external view returns (uint256) {
         return stETH.getPooledEthByShares(_wstETHAmount);
     }
 
     /**
-     * @return Returns amount of stETH for 1 wstETH
+     * @notice Get amount of wstETH for a one stETH
+     * @return Amount of stETH for 1 wstETH
      */
     function stEthPerToken() external view returns (uint256) {
         return stETH.getPooledEthByShares(1 ether);
     }
 
     /**
-     * @return Returns amount of wstETH for a 1 stETH
+     * @notice Get amount of stETH for a one wstETH
+     * @return Amount of wstETH for a 1 stETH
      */
     function tokensPerStEth() external view returns (uint256) {
         return stETH.getSharesByPooledEth(1 ether);
