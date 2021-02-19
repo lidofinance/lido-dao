fbc62a8fb39a9c63577b2bb66976c0efbff7f711
Eugene Pshenichnyy <pshenichniy.eugene@gmail.com>
2021-02-19 20:54:49 +0300

    feat: mainnet deploy

--- scripts/deploy-wsteth.js
+++ scripts/deploy-wsteth.js
@@ -1,22 +1,26 @@
 const chalk = require('chalk')
+const hre = require('hardhat')
 
 const runOrWrapScript = require('./helpers/run-or-wrap-script')
 const { log, logSplitter, logWideSplitter } = require('./helpers/log')
 const { deploy, withArgs } = require('./helpers/deploy')
 const { readNetworkState, persistNetworkState } = require('./helpers/persisted-network-state')
 
-const NETWORK_STATE_FILE = process.env.NETWORK_STATE_FILE || 'deployed.json'
-
-async function deployWstEthContract({ web3, artifacts, networkStateFile = NETWORK_STATE_FILE }) {
+async function deployWstEthContract({ web3, artifacts }) {
   const netId = await web3.eth.net.getId()
+  const network = hre.network.name
 
   logWideSplitter()
   log(`Network ID: ${chalk.yellow(netId)}`)
+  log(`Network name: ${chalk.yellow(network)}`)
 
-  const state = readNetworkState(networkStateFile, netId)
+  const state = readNetworkState(network, netId)
   const [deployer] = await web3.eth.getAccounts()
 
   const stEthAddress = state['app:lido'].proxyAddress
+  console.log('stEthAddress', stEthAddress)
+  console.log('deployer', deployer)
+
   const wstethContractResults = await deployWsteth({
     artifacts,
     stEthAddress,
@@ -24,7 +28,7 @@ async function deployWstEthContract({ web3, artifacts, networkStateFile = NETWOR
   })
 
   logSplitter()
-  persistNetworkState(networkStateFile, netId, state, wstethContractResults)
+  persistNetworkState(network, netId, state, wstethContractResults)
 }
 
 async function deployWsteth({ artifacts, stEthAddress, deployer }) {
