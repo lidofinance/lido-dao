642997d4c6a3bdf16717464390b2dfd717a81521
Tom French <tom@tomfren.ch>
2021-05-25 00:57:51 +0100

    feat: allow buildKeyData to handle merkle trees with a single leaf

--- test/0.4.24/helpers/keyData.js
+++ test/0.4.24/helpers/keyData.js
@@ -5,11 +5,10 @@ const buildKeyData = (operators, operatorId, leafIndex) => {
   const operator = operators[operatorId]
   const publicKeys = hexConcat(...operator.keys[leafIndex])
   const signatures = hexConcat(...operator.sigs[leafIndex])
-  const proofData = hexConcat(
-    ...MerkleTree.fromKeysAndSignatures(operator.keys, operator.sigs).getProof(
-      calcLeafHash(operator.keys[leafIndex], operator.sigs[leafIndex])
-    )
+  const merkleProof = MerkleTree.fromKeysAndSignatures(operator.keys, operator.sigs).getProof(
+    calcLeafHash(operator.keys[leafIndex], operator.sigs[leafIndex])
   )
+  const proofData = merkleProof.length > 0 ? hexConcat(...merkleProof) : '0x'
   return {
     operatorId,
     leafIndex,
