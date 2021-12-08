33539f06ad6ca96405e7d08d374d3a740835978d
Logachev Nikita <logach3@gmail.com>
2021-12-08 11:37:29 +0300

    Add version to lido-cli

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"lido-cli/pkg/apps"
 	"lido-cli/pkg/aragon"
 	"lido-cli/pkg/contracts"
@@ -10,6 +11,11 @@ import (
 	"lido-cli/pkg/logs"
 
 	"github.com/spf13/cobra"
+)
+
+var (
+	version  string
+	revision string
 )
 
 type LidoExecutor struct {
@@ -51,13 +57,15 @@ var (
 // Execute executes the root command.
 func Execute() error {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.Version = fmt.Sprintf("%s (rev-%s)\n", version, revision)
 	rootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
 
 	rootCmd.PersistentFlags().StringVar(&Lido.NetworkName, "network", "localhost", "Set deploy network name")
 	rootCmd.PersistentFlags().BoolVarP(&logs.Verbose, "verbose", "v", false, "Verbose output all of services")
 	rootCmd.PersistentFlags().StringVar(&Lido.LidoApps.AppsLocator, "apps", "", "Which source to load app frontend assets from")
 	rootCmd.PersistentFlags().StringVar(&Lido.LidoApps.Path, "apps-path", "", "Lido apps path")
-	rootCmd.PersistentFlags().StringVar(&Lido.HardhatNode.Fork, "fork", "", "Fork endpoint ttps://mainnet.infura.io/v3/{WEB3_INFURA_PROJECT_ID}")
+	rootCmd.PersistentFlags().StringVar(&Lido.HardhatNode.Fork, "fork", "", "Fork endpoint https://mainnet.infura.io/v3/{WEB3_INFURA_PROJECT_ID}")
+	// rootCmd.PersistentFlags().StringVar(&Lido.HardhatNode.Fork, "version", "", "Tool version")
 
 	return rootCmd.Execute()
 }
--- cli/main.go
+++ cli/main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"lido-cli/cmd"
 )
 
-var (
-	version  string
-	revision string
-)
-
 func main() {
-	fmt.Printf("lido-cli %s (rev-%s)\n", version, revision)
-
-	// pterm.Success = *pterm.Success.WithPrefix(pterm.Prefix{Text: fmt.Sprint(chalk.Green, "  ✔"), Style: pterm.DefaultBasicText.Style}).WithMessageStyle(&pterm.ThemeDefault.DescriptionMessageStyle)
-	// pterm.Error = *pterm.Error.WithPrefix(pterm.Prefix{Text: fmt.Sprint(chalk.Red, "  ✖"), Style: pterm.DefaultBasicText.Style}).WithMessageStyle(&pterm.ThemeDefault.DescriptionMessageStyle)
-	// pterm.Info = *pterm.Info.WithPrefix(pterm.Prefix{Text: fmt.Sprint(chalk.Yellow, "  ↓"), Style: pterm.DefaultBasicText.Style}).WithMessageStyle(&pterm.ThemeDefault.DescriptionMessageStyle)
-
 	defer cmd.Lido.Shutdown()
 
 	cmd.Execute()
-
 }
