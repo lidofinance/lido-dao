3339cafdfcf2066035e15643694232e614de8d11
KRogLA <i@krogla.me>
2021-12-24 11:51:44 +0100

    fix std aragon apps deploy

--- kintsugi-local-setup.sh
+++ kintsugi-local-setup.sh
@@ -2,8 +2,9 @@
 
 rm -f deployed-kl.json
 # echo "{\"depositContractAddress\":\"0x4242424242424242424242424242424242424242\"}" > deployed-kl.json
+yarn compile
 yarn deploy:kl:aragon-env
 yarn deploy:kl:aragon-std-apps
 yarn deploy:kl:apm-and-template
 yarn deploy:kl:apps
-# yarn deploy:kl:dao
+yarn deploy:kl:dao
