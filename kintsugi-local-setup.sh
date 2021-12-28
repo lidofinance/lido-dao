46a98acea34d9c34ee66769af66e87659dba774c
KRogLA <i@krogla.me>
2021-12-28 14:09:01 +0100

    fix local deploy

--- kintsugi-local-setup.sh
+++ kintsugi-local-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 rm -f deployed-kl.json
-# echo "{\"depositContractAddress\":\"0x4242424242424242424242424242424242424242\"}" > deployed-kl.json
 yarn compile
 yarn deploy:kl:aragon-env
 yarn deploy:kl:aragon-std-apps
