411b2cd1ca6cb7690e386cd7480b6b7026f72d87
KRogLA <i@krogla.me>
2022-01-26 17:14:22 +0100

    fix: reduce block geth time, minor fixes

--- dao-local-deploy.sh
+++ dao-local-deploy.sh
@@ -3,65 +3,71 @@ set -e +u
 set -o pipefail
 
 # first local account by default
-DEPLOYER=0xb4124cEB3451635DAcedd11767f004d8a28c6eE7
+DEPLOYER=${DEPLOYER:=0xb4124cEB3451635DAcedd11767f004d8a28c6eE7}
 # NETWORK=kintsugi
-NETWORK=local
+NETWORK=${NETWORK:=local}
+
+function msg() {
+  MSG=$1
+  if [ ! -z "$MSG" ]; then
+    echo ">>> ============================="
+    echo ">>> $MSG"
+    echo ">>> ============================="
+  fi
+}
 
 function pause() {
   MSG=$1
-  if [ ! -z "$MSG" ]; then
-    echo "$MSG"
-  fi
+  msg "$1"
   read -s -n 1 -p "Press any key to continue . . ."
   echo ""
 }
 
 docker-compose down -v
-docker-compose up -d
+docker-compose up --build -d
 
 rm -f deployed-$NETWORK.json
 cp deployed-local-defaults.json deployed-$NETWORK.json
 
 yarn compile
 yarn deploy:$NETWORK:aragon-env
-pause "Aragon ENV deployed."
+msg "Aragon ENV deployed."
 yarn deploy:$NETWORK:aragon-std-apps
-pause "Aragon STD apps deployed."
+msg "Aragon STD apps deployed."
 
 yarn hardhat --network $NETWORK run --no-compile ./scripts/deploy-beacon-deposit-contract.js
-pause "Deposit contract deployed."
+msg "Deposit contract deployed."
 
 yarn hardhat --network $NETWORK run ./scripts/multisig/01-deploy-lido-template-and-bases.js
 yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-01-1-deploy-template.json
-pause "!!! now set the daoTemplateDeployTx hash value in deployed-$NETWORK.json"
+pause "!!! Now set the daoTemplateDeployTx hash value in deployed-$NETWORK.json"
 yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-01-2-deploy-lido-base.json
-pause "!!! now set the lidoBaseDeployTx hash value in deployed-$NETWORK.json"
+pause "!!! Now set the lidoBaseDeployTx hash value in deployed-$NETWORK.json"
 yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-01-3-deploy-oracle-base.json
-pause "!!! now set the oracleBaseDeployTx hash value in deployed-$NETWORK.json"
+pause "!!! Now set the oracleBaseDeployTx hash value in deployed-$NETWORK.json"
 yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-01-4-deploy-nops-base.json
-pause "!!! now set the nodeOperatorsRegistryBaseDeployTx hash value in deployed-$NETWORK.json"
+pause "!!! Now set the nodeOperatorsRegistryBaseDeployTx hash value in deployed-$NETWORK.json"
 yarn hardhat --network $NETWORK run ./scripts/multisig/02-obtain-deployed-instances.js
-pause "Apps instances deployd"
-# pause
+msg "Apps instances deployd"
+
 yarn hardhat --network $NETWORK run ./scripts/multisig/03-register-ens-domain.js
 if [ -f "tx-02-1-commit-ens-registration.json" ]; then
   yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-02-1-commit-ens-registration.json
-  pause
 fi
 yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-02-2-make-ens-registration.json
-pause "ENS registered"
+msg "ENS registered"
+
 yarn hardhat --network $NETWORK run ./scripts/multisig/04-publish-app-frontends.js
-pause "Frontend published to IPFS"
+msg "Frontend published to IPFS"
 
 yarn hardhat --network $NETWORK run ./scripts/multisig/05-deploy-apm.js
 yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-03-deploy-apm.json
-pause "!!! now set the lidoApmDeployTx hash value in deployed-$NETWORK.json"
-
 yarn hardhat --network $NETWORK run ./scripts/multisig/06-obtain-deployed-apm.js
-pause "APM deployed"
+msg "APM deployed"
+
 yarn hardhat --network $NETWORK run ./scripts/multisig/07-create-app-repos.js
 yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-04-create-app-repos.json
-pause "App repos created"
+msg "App repos created"
 
 yarn hardhat --network $NETWORK run ./scripts/multisig/08-deploy-dao.js
 yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-05-deploy-dao.json
@@ -70,13 +76,15 @@ pause "DAO deploy started"
 
 yarn hardhat --network $NETWORK run ./scripts/multisig/10-issue-tokens.js
 yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-06-1-issue-tokens.json
-pause "Tokens issued"
+msg "Tokens issued"
 
 yarn hardhat --network $NETWORK run ./scripts/multisig/11-finalize-dao.js
 yarn hardhat --network $NETWORK tx --from $DEPLOYER --file tx-07-finalize-dao.json
-pause "DAO deploy finalized"
+msg "DAO deploy finalized"
 
 yarn hardhat --network $NETWORK run ./scripts/multisig/12-check-dao.js
+msg "Check completed! Clening up..."
 
-echo "Check completed!"
+rm tx-*.json
+msg "Deploy completed!"
 
