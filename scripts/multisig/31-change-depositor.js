606da66fbdf414667eff24b02c6e6e8b28912caf
George Avsetsin <mail@sjors.ru>
2022-05-23 20:47:25 +0300

    feat: move LastDepositBlock forward

--- scripts/multisig/31-change-depositor.js
+++ scripts/multisig/31-change-depositor.js
@@ -29,6 +29,9 @@ async function changeDepositor({ web3, artifacts }) {
 
   logSplitter()
 
+  const averageBlocksPerSecond = 13.8235 // average speed based on 14700000...14830000 range
+  const votingDurationBlocks = Math.floor((60 * 60 * 24 * 3) / averageBlocksPerSecond) // 3 days
+
   const votingAddress = state[`app:${APP_NAMES.ARAGON_VOTING}`].proxyAddress
   const tokenManagerAddress = state[`app:${APP_NAMES.ARAGON_TOKEN_MANAGER}`].proxyAddress
   const lidoAddress = state[`app:${APP_NAMES.LIDO}`].proxyAddress
@@ -43,14 +46,18 @@ async function changeDepositor({ web3, artifacts }) {
   const depositRoleEvents = await acl.getPastEvents('SetPermission', { filter, fromBlock: 0 })
   const oldDepositorAddress = depositRoleEvents.sort((a, b) => b.blockNumber - a.blockNumber)[0].returnValues.entity
   const newDepositorAddress = state.depositorAddress
+  const newDepositor = await artifacts.require('DepositSecurityModule').at(newDepositorAddress)
+  const currentBLock = await await web3.eth.getBlockNumber()
+  const newLastDepositBlock = currentBLock + votingDurationBlocks
 
   log(`Using ENS:`, yl(state.ensAddress))
   log(`TokenManager address:`, yl(tokenManagerAddress))
   log(`Voting address:`, yl(votingAddress))
-  log(`Kernel`, yl(kernel.address))
-  log(`ACL`, yl(acl.address))
-  log(`Old Depositor`, yl(oldDepositorAddress))
-  log(`New Depositor`, yl(newDepositorAddress))
+  log(`Kernel:`, yl(kernel.address))
+  log(`ACL:`, yl(acl.address))
+  log(`Old Depositor:`, yl(oldDepositorAddress))
+  log(`New Depositor:`, yl(newDepositorAddress))
+  log(`LastDepositBlock:`, yl(newLastDepositBlock))
 
   log.splitter()
 
@@ -64,7 +71,12 @@ async function changeDepositor({ web3, artifacts }) {
     calldata: await acl.contract.methods.grantPermission(newDepositorAddress, state[`app:lido`].proxyAddress, depositRole).encodeABI()
   }
 
-  const encodedUpgradeCallData = encodeCallScript([revokeCallData, grantCallData])
+  const setLastBlockCallData = {
+    to: newDepositorAddress,
+    calldata: await newDepositor.contract.methods.setLastDepositBlock(newLastDepositBlock).encodeABI()
+  }
+
+  const encodedUpgradeCallData = encodeCallScript([revokeCallData, grantCallData, setLastBlockCallData])
 
   log(`encodedUpgradeCallData:`, yl(encodedUpgradeCallData))
   const votingCallData = encodeCallScript([
@@ -78,6 +90,7 @@ async function changeDepositor({ web3, artifacts }) {
   const votingDesc = `
     1) Revoke permission DEPOSIT_ROLE from ${oldDepositorAddress}
     2) Grant permission DEPOSIT_ROLE to ${newDepositorAddress}
+    3) Set last deposit block to ${newLastDepositBlock}
   `
 
   await saveCallTxData(votingDesc, tokenManager, 'forward', txName, {
