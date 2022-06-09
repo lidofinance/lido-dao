f63033c0051cc4b23a0f63afef3f331eff74686c
Azat Serikov <azatsdev@gmail.com>
2022-06-09 12:52:53 +0600

    feat: update staking limit block

--- apps/lido/app/src/components/StakingLimitState.js
+++ apps/lido/app/src/components/StakingLimitState.js
@@ -1,31 +1,50 @@
 import { useAppState } from '@aragon/api-react'
 import { Box } from '@aragon/ui'
 import React from 'react'
-import { ListItem, LoadableElement } from './shared'
+import { ListItem, LoadableElement, RestorationRate, Tooltip } from './shared'
 import { Ether } from './shared/Ether'
 
 export const StakingLimitState = () => {
   const { stakingLimitInfo } = useAppState()
 
   return (
-    <Box heading="Staking">
+    <Box heading="Staking status">
       <ListItem label="Paused">
         <LoadableElement value={stakingLimitInfo?.isStakingPaused}>
           {stakingLimitInfo?.isStakingPaused ? 'Yes' : 'No'}
         </LoadableElement>
       </ListItem>
-      <ListItem label="Limit set">
+      <ListItem
+        label={
+          <Tooltip tooltip="Staking limit is the total amount of ether that can be staked in a given block. The limit goes down with each deposit but is passively restored on each block.">
+            Limit set
+          </Tooltip>
+        }
+      >
         <LoadableElement value={stakingLimitInfo?.isStakingLimitSet}>
           {stakingLimitInfo?.isStakingLimitSet ? 'Yes' : 'No'}
         </LoadableElement>
       </ListItem>
-      <ListItem label="Max limit">
+      <ListItem
+        label={
+          <Tooltip tooltip="Staking limit will not be restored past this limit.">
+            Max limit
+          </Tooltip>
+        }
+      >
         <Ether ether={stakingLimitInfo?.maxStakeLimit} />
       </ListItem>
-      <ListItem label="Restoration rate">
-        <LoadableElement value={stakingLimitInfo?.maxStakeLimitGrowthBlocks}>
-          {stakingLimitInfo?.maxStakeLimitGrowthBlocks} blocks
-        </LoadableElement>
+      <ListItem
+        label={
+          <Tooltip tooltip="Rate at which staking limit is passively restored. For example, a restoration rate of 150,000 ether per 6240 blocks means that the staking limit goes up by ">
+            Restoration
+          </Tooltip>
+        }
+      >
+        <RestorationRate
+          maxLimit={stakingLimitInfo?.maxStakeLimit}
+          blocks={stakingLimitInfo?.maxStakeLimitGrowthBlocks}
+        />
       </ListItem>
     </Box>
   )
