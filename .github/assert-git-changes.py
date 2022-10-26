94dc1c1ec42fb27d18359502c47dcd5e939195b1
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-10-26 17:35:48 +0400

    fix: assert-git-changes.py

--- .github/assert-git-changes.py
+++ .github/assert-git-changes.py
@@ -4,12 +4,12 @@ import os
 
 target_dir = os.environ.get("TARGET_DIR")
 
-if not target_dir:
-    print("No TARGET_DIR env variable provided. Exiting")
-    exit(1)
-
 git_changes = subprocess.getoutput("git status --porcelain")
 print(f"Changes:\n{git_changes}")
+
+if not target_dir and git_changes:
+    print(f"Changes detected! Failing")
+    exit(1)
 
 if git_changes.find(target_dir) > 0:
     print(f"Changes in {target_dir} detected! Failing")
