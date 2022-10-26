b9eda5ce72ec7494ae3f84c9c82f72b7e9f0e8f1
Dmitry Gusakov <gusakov.dv@gmail.com>
2022-10-26 13:46:54 +0400

    chore(security): Add storage layout linter

--- .github/assert-git-changes.py
+++ .github/assert-git-changes.py
@@ -4,13 +4,15 @@ import os
 
 target_dir = os.environ.get("TARGET_DIR")
 
-if not target_dir:
-    print("No TARGET_DIR env variable provided. Exiting")
-    exit(1)
-
 git_changes = subprocess.getoutput("git status --porcelain")
-print(f"Changes:\n{git_changes}")
 
-if git_changes.find(target_dir) > 0:
-    print(f"Changes in {target_dir} detected! Failing")
-    exit(1)
+if git_changes:
+    print(f"Changes:\n{git_changes}")
+    if not target_dir:
+        print(f"Changes detected! Failing")
+        exit(1)
+    elif git_changes.find(target_dir) > 0:
+        print(f"Changes in {target_dir} detected! Failing")
+        exit(1)
+else:
+    print(f"No changes detected")
