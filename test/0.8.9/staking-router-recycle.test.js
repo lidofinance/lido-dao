1c4f9d5770e1d9781efab67cc8ef3820c0c96dfb
KRogLA <i@krogla.me>
2022-12-16 13:21:16 +0100

    feat: SR simple recycle

--- test/0.8.9/staking-router-recycle.test.js
+++ test/0.8.9/staking-router-recycle.test.js
@@ -77,7 +77,7 @@ const soloModule2 = {
   type: 2, // DVT
   fee: 800, // in basic points
   treasuryFee: 200, // in basic points
-  targetShare: 100, // 1%
+  targetShare: 200, // 2%
   recycleShare: 10000, // +100% of targetShare
   assignedDeposits: 0,
   balance: 0,
@@ -178,7 +178,7 @@ contract('StakingRouter', (accounts) => {
       assertBn(500, NORFee, 'invalid node operator registry fee')
     })
 
-    it.skip(`base functions`, async () => {
+    it(`base functions`, async () => {
       // 50% of mintedShares
       await operators.setFee(500, { from: appManager })
 
@@ -364,21 +364,6 @@ contract('StakingRouter', (accounts) => {
       const stakeModules = [proModule, soloModule, soloModule2]
       const stakeModuleContracts = []
 
-      stakeModules[0].totalKeys = 14900
-      stakeModules[0].totalUsedKeys = 9700
-      stakeModules[0].totalStoppedKeys = 0
-      stakeModules[0].targetShare = 10000
-
-      stakeModules[1].totalKeys = 400
-      stakeModules[1].totalUsedKeys = 100
-      stakeModules[1].totalStoppedKeys = 0
-      stakeModules[1].targetShare = 300
-
-      stakeModules[2].totalKeys = 300
-      stakeModules[2].totalUsedKeys = 0
-      stakeModules[2].totalStoppedKeys = 0
-      stakeModules[2].targetShare = 200
-
       log('add node operator to curated module')
       await operators.addNodeOperator('op1', ADDRESS_1, { from: voting })
       let totalAllocation = 0
@@ -430,7 +415,7 @@ contract('StakingRouter', (accounts) => {
       // await getLidoStats(lido, { Stranger1: externalAddress, Stranger2: stranger2, StakingRouter: stakingRouter.address })
 
       log('simulate balance topup')
-      const depositsAmount = 200
+      const depositsAmount = 101
       await provider.send('hardhat_setBalance', [stakingRouter.address, '0x' + parseInt(ETH(depositsAmount * 32)).toString(16)])
 
       logHeader('+1h 1st distributeDeposits')
@@ -442,7 +427,7 @@ contract('StakingRouter', (accounts) => {
       await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 0, 0] })
       await getAndCheckRecAlloc(stakingRouter, {
         totalRecycleKeys: 0,
-        recycleLevels: [0, 0, 0],
+
         recycleKeys: [0, 0, 0]
       })
 
@@ -455,10 +440,9 @@ contract('StakingRouter', (accounts) => {
 
       // after allocation
       await getModulesInfo(stakingRouter)
-      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 50, 150] })
+      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 16, 85] })
       await getAndCheckRecAlloc(stakingRouter, {
         totalRecycleKeys: 0,
-        recycleLevels: [0, 0, 0],
         recycleKeys: [0, 0, 0]
       })
 
@@ -478,20 +462,18 @@ contract('StakingRouter', (accounts) => {
       // newTotalAllocation = curTotalAllocation + depositsAmount = 10068 + 101 = 10169
 
       // maxAssignedKeys[i] = min(newTotalAllocation * targetShare[i], totalUsedKeys[i]) - curAllocation[i]
-      // maxAssignedKeys[0] = min(10169 * 1, 9999) - 9998 = min(10169,9999) - 9998 = 1
-      // maxAssignedKeys[1] = min(10169 * 0.01, 300) - 70 = min(100,300) - 70 = 30
-      // maxAssignedKeys[0] = min(10169 * 0.02, 100) - 0 = min(200,100) - 0 = 100
+      // maxAssignedKeys[0] = min(10169 * 1, 9999) - 9998 = min(10169,10099) - 9998 = 101
+      // maxAssignedKeys[1] = min(10169 * 0.01, 300) - 70 = min(101,300) - 70 = 32
+      // maxAssignedKeys[2] = min(10169 * 0.02, 100) - 0 = min(202,100) - 0 = 100
       // 1st iteration: lowest stake has module[2], so at least 70 keys will be assigned to be equal to module[1]
       // 2nd iteration: rest (101 - 70) = 31 keys divided between module[1] (+15) and  module[2] (+15), reminder goes to module[1] (+1) as it has lower index
       // assignedKeys[0] = 0
       // assignedKeys[1] = 15 + 1 = 16
       // assignedKeys[2] = 70 + 15 = 85
       await getModulesInfo(stakingRouter)
-      // await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 16, 85] })
-      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 50, 150] })
+      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 16, 85] })
       await getAndCheckRecAlloc(stakingRouter, {
         totalRecycleKeys: 0,
-        recycleLevels: [0, 0, 0],
         recycleKeys: [0, 0, 0]
       })
 
@@ -507,44 +489,37 @@ contract('StakingRouter', (accounts) => {
 
       await getModulesInfo(stakingRouter)
       // assignedKeys[1] = 16 - 3(deposited) = 13
-      // alloc = await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 13, 85] })
-      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 40, 150] })
+      alloc = await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 6, 85] })
       await getAndCheckRecAlloc(stakingRouter, {
         totalRecycleKeys: 0,
-        recycleLevels: [0, 0, 0],
         recycleKeys: [0, 0, 0]
       })
 
       // wait +6 hours, 50% of keys avail to recycle
       logHeader('forward +12h after 1st distributeDeposits')
-      log('- module0 idle 12h, no keys, skip recycle;\n- module1 idle 6h (after deposit), skip recycle;\n- module2 idle 12h, 50% recycle')
+      log('- module0 idle 12h, no keys, skip recycle;\n- module1 idle 6h (after deposit), skip recycle;\n- module2 idle 12h, 100% recycle')
 
       await provider.send('evm_increaseTime', [3600 * 6 + 10])
       await provider.send('evm_mine')
 
       await getModulesInfo(stakingRouter)
-      // await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 13, 85] })
-      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 40, 150] })
+      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 6, 85] })
+
       // recycledKeys[0] = 0, it doesn't have any allocation
       // recycledKeys[1] = 0, less than 12h pass since module deposited
-      // recycledKeys[2] = 50% of 85 = 42, more than 12h (but less than 15h) pass since distribution and module not yet deposited
+      // recycledKeys[2] = 100% of 85 = 85
       await getAndCheckRecAlloc(stakingRouter, {
-        // totalRecycleKeys: 42,
-        // recycleLevels: [0, 0, 1],
-        // recycleKeys: [0, 0, 42]
-        totalRecycleKeys: 75,
-        recycleLevels: [0, 0, 1],
-        recycleKeys: [0, 0, 75]
+        totalRecycleKeys: 85,
+        recycleKeys: [0, 0, 85]
       })
 
       // at this point module1 is 'good' because it's last deposit was 6h ago
+      // hardCap = newTotalAllocation * targetShare * ( 1 + recycleShare) - usedKeys - assignedKeys
+      // hardCap[1] = 10169 * 0.01 * 1.5 - 90 - 6 = 152 - 90 - 6 = 56
       await getAndCheckMaxKeys(stakingRouter, stakeModules, [
-        // { assignedKeys: 0, recycledKeys: 42 }, // min(totalKeys-usedKeys, 50% keys of module1+module2)
-        // { assignedKeys: 13, recycledKeys: 42 }, // 50% keys of module 2 + module 0
-        // { assignedKeys: 85, recycledKeys: 0 } // 50% keys of module 1 + module 0
-        { assignedKeys: 0, recycledKeys: 75 }, // min(totalKeys-usedKeys, 50% keys of module1+module2)
-        { assignedKeys: 40, recycledKeys: 75 }, // 50% keys of module 2 + module 0
-        { assignedKeys: 150, recycledKeys: 0 } // 50% keys of module 1 + module 0
+        { assignedKeys: 0, recycledKeys: 85 }, // min(totalKeys-usedKeys, 100% keys of module1+module2)
+        { assignedKeys: 6, recycledKeys: 56 }, // 100% keys of module 2 + module 0, but no more than hardCap
+        { assignedKeys: 85, recycledKeys: 0 } // 100% keys of module 1 + module 0
       ])
 
       logWideSplitter(bl('call distributeDeposits()...'))
@@ -555,38 +530,28 @@ contract('StakingRouter', (accounts) => {
 
       await getModulesInfo(stakingRouter)
       // await getAndCheckAlloc(stakingRouter, { assignedKeys:  [0, 13, 85]})
-      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 40, 150] })
+      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 6, 85] })
       await getAndCheckRecAlloc(stakingRouter, {
-        // totalRecycleKeys: 42,
-        // recycleLevels: [0, 0, 1],
-        // recycleKeys: [0, 0, 42]
-        totalRecycleKeys: 75,
-        recycleLevels: [0, 0, 1],
-        recycleKeys: [0, 0, 75]
+        totalRecycleKeys: 85,
+        recycleKeys: [0, 0, 85]
       })
 
       assert.equal((await stakingRouter.lastDistributeAt()).toNumber(), lastDistributeAt)
 
-      // keysAmount = 20
-      keysAmount = 75
+      keysAmount = 25
       moduleId = 0
       logSplitter()
-      // await logTx(bl(`call deposit(): Module #${moduleId}, keys: ${keysAmount}`), stakeModuleContracts[moduleId].deposit(keysAmount))
-      await logTx(bl(`call deposit(): Module #${moduleId}, keys: ${keysAmount}`), stakeModuleContracts[moduleId].deposit(25))
-      await logTx(bl(`call deposit(): Module #${moduleId}, keys: ${keysAmount}`), stakeModuleContracts[moduleId].deposit(25))
-      await logTx(bl(`call deposit(): Module #${moduleId}, keys: ${keysAmount}`), stakeModuleContracts[moduleId].deposit(25))
+      // woarkaround to not increase block baseFee during the tests
+      await logTx(bl(`call deposit(): Module #${moduleId}, keys: ${keysAmount}`), stakeModuleContracts[moduleId].deposit(keysAmount))
+      await logTx(bl(`call deposit(): Module #${moduleId}, keys: ${keysAmount}`), stakeModuleContracts[moduleId].deposit(keysAmount))
+      await logTx(bl(`call deposit(): Module #${moduleId}, keys: ${keysAmount}`), stakeModuleContracts[moduleId].deposit(keysAmount))
 
       await getModulesInfo(stakingRouter)
-      // await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 13, 65] })// recycled keys are subtracted from module[2] allocation
-      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 40, 75] })
-      // module[0] deposited 20 keys which will be taken from module[2]
+      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 6, 10] }) // recycled keys are subtracted from module[2] allocation
+      // module[0] deposited 75 keys which will be taken from module[2]
       await getAndCheckRecAlloc(stakingRouter, {
-        // totalRecycleKeys: 22, // 42 - 20
-        // recycleLevels: [0, 0, 1],
-        // recycleKeys: [0, 0, 22]
-        totalRecycleKeys: 0,
-        recycleLevels: [0, 0, 1],
-        recycleKeys: [0, 0, 0]
+        totalRecycleKeys: 10, // 85 - 75
+        recycleKeys: [0, 0, 10]
       })
 
       logWideSplitter(bl('call distributeDeposits()...'))
@@ -594,56 +559,23 @@ contract('StakingRouter', (accounts) => {
       await assertRevert(stakingRouter.distributeDeposits(), revertReason)
       log(OK, `- ${rd('revert')}!, reason: ${yl(revertReason)}`)
 
-      logHeader('forward +15h after 1st distributeDeposits')
-      log('- module0 idle 3h, no keys, skip recycle;\n- module1 idle 9h (after deposit), skip recycle;\n- module2 idle 15h, 75% recycle')
-
-      await provider.send('evm_increaseTime', [3600 * 3 + 10])
-      await provider.send('evm_mine')
-
-      await getModulesInfo(stakingRouter)
-      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 40, 75] })
-      await getAndCheckRecAlloc(stakingRouter, {
-        // totalRecycleKeys: 22,
-        // recycleLevels: [0, 0, 2],
-        // recycleKeys: [0, 0, 22]
-        totalRecycleKeys: 56,
-        recycleLevels: [0, 0, 2],
-        recycleKeys: [0, 0, 56]
-      })
-
-      await getAndCheckMaxKeys(stakingRouter, stakeModules, [
-        // { assignedKeys: 0, recycledKeys: 22 }, // 75% keys of module 2
-        // { assignedKeys: 48, recycledKeys: 22 }, // 75% keys of module 2
-        // { assignedKeys: 30, recycledKeys: 0 } // no recycle here due to: module 1 has deposited 9h ago, and mod 0 have no allocation
-        { assignedKeys: 0, recycledKeys: 56 }, // 75% keys of module 2
-        { assignedKeys: 40, recycledKeys: 56 }, // 75% keys of module 2
-        { assignedKeys: 75, recycledKeys: 0 } // no recycle here due to: module 1 has deposited 9h ago, and mod 0 have no allocation
-      ])
-
       logHeader('forward +18h after 1st distributeDeposits')
-      log('- module0 idle 6h, no keys, skip recycle;\n- module1 idle 12h (after deposit), 50% recycle;\n- module2 idle 18h, 100% recycle')
+      log('- module0 idle 6h, no keys, skip recycle;\n- module1 idle 12h (after deposit), 100% recycle;\n- module2 idle 18h, 100% recycle')
 
-      await provider.send('evm_increaseTime', [3600 * 3 + 10])
+      await provider.send('evm_increaseTime', [3600 * 6 + 10])
       await provider.send('evm_mine')
 
       await getModulesInfo(stakingRouter)
-      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 40, 75] })
+      await getAndCheckAlloc(stakingRouter, { assignedKeys: [0, 6, 10] })
       await getAndCheckRecAlloc(stakingRouter, {
-        // totalRecycleKeys: 54,
-        // recycleLevels: [0, 1, 3],
-        // recycleKeys: [0, 24, 30]
-        totalRecycleKeys: 95,
-        recycleLevels: [0, 1, 3],
-        recycleKeys: [0, 20, 75]
+        totalRecycleKeys: 16,
+        recycleKeys: [0, 6, 10]
       })
 
       await getAndCheckMaxKeys(stakingRouter, stakeModules, [
-        // { assignedKeys: 0, recycledKeys: 26 }, // totalKeys-usedKeys < 100% recycle keys of module2+ 50% keys of module 1)
-        // { assignedKeys: 48, recycledKeys: 30 }, // 100% keys of mod 2
-        // { assignedKeys: 30, recycledKeys: 24 } // 50% keys of module 1
-        { assignedKeys: 0, recycledKeys: 95 }, // totalKeys-usedKeys < 100% recycle keys of module2+ 50% keys of module 1)
-        { assignedKeys: 40, recycledKeys: 75 }, // 100% keys of mod 2
-        { assignedKeys: 75, recycledKeys: 20 } // 50% keys of module 1
+        { assignedKeys: 0, recycledKeys: 16 }, // totalKeys-usedKeys < 100% recycle keys of module2+ 100% keys of module 1)
+        { assignedKeys: 6, recycledKeys: 10 }, // 100% keys of mod 2
+        { assignedKeys: 10, recycledKeys: 6 } // 100% keys of module 1
       ])
     })
   })
@@ -693,18 +625,15 @@ async function getAndCheckRecAlloc(stakingRouter, recAllocCheck = undefined) {
   const _recAlloc = await stakingRouter.getRecycleAllocation()
 
   recAlloc.totalRecycleKeys = +_recAlloc.totalRecycleKeys
-  recAlloc.recycleLevels = []
   recAlloc.recycleKeys = []
   for (let i = 0; i < modulesCount; i++) {
-    recAlloc.recycleLevels.push(+_recAlloc.recycleLevels[i])
     recAlloc.recycleKeys.push(+_recAlloc.recycleKeys[i])
   }
   log('>>> Recycle allocation <<<')
   logTable(recAlloc)
   if (recAllocCheck) {
-    const { totalRecycleKeys, recycleLevels, recycleKeys } = recAllocCheck
+    const { totalRecycleKeys, recycleKeys } = recAllocCheck
     assert.equal(recAlloc.totalRecycleKeys, totalRecycleKeys)
-    assert.deepEqual(recAlloc.recycleLevels, recycleLevels)
     assert.deepEqual(recAlloc.recycleKeys, recycleKeys)
   }
   return recAlloc
@@ -744,9 +673,6 @@ async function getModulesInfo(stakingRouter) {
       paused: comModule.paused,
       active: comModule.active,
       lastDepositAt: +comModule.lastDepositAt,
-      recycleAt: +comModule.recycleAt,
-      recycleLevel: +comModule.recycleLevel,
-      recycleRestKeys: +comModule.recycleRestKeys,
 
       totalKeys: +(await entry.getTotalKeys()),
       totalUsedKeys: +(await entry.getTotalUsedKeys()),
