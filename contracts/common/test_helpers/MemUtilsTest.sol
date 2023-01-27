lidofinance/lido-dao

commit e5f7e4dfd6d26eba5893ee3a585631bf806d928e

Gas optimisation at memutils

contracts/0.4.24/lib/MemUtils.sol
@@ -33,7 +33,7 @@ library MemUtils {
             if gt(_len, 0) {
                 // read the next 32-byte chunk from _dst, replace the first N bytes
                 // with those left in the _src, and write the transformed chunk back
-                let mask := sub(exp(256, sub(32, _len)), 1) // 2 ** (8 * (32 - _len)) - 1
+                let mask := sub(shl(1, mul(8, sub(32, _len))), 1) // 2 ** (8 * (32 - _len)) - 1
                 let srcMasked := and(mload(_src), not(mask))
                 let dstMasked := and(mload(_dst), mask)
                 mstore(_dst, or(dstMasked, srcMasked))

commit 3b431b5aab90034f4141ca4f929413ae93077ead

fix a memcpy error introduced in e5f7e4d, cover MemUtils w/tests

contracts/common/lib/MemUtils.sol
@@ -33,7 +33,7 @@ library MemUtils {
             if gt(_len, 0) {
                 // read the next 32-byte chunk from _dst, replace the first N bytes
                 // with those left in the _src, and write the transformed chunk back
-                let mask := sub(shl(1, mul(8, sub(32, _len))), 1) // 2 ** (8 * (32 - _len)) - 1
+                let mask := sub(shl(mul(8, sub(32, _len)), 1), 1) // 2 ** (8 * (32 - _len)) - 1
                 let srcMasked := and(mload(_src), not(mask))
                 let dstMasked := and(mload(_dst), mask)
                 mstore(_dst, or(dstMasked, srcMasked))

contracts/common/test_helpers/MemUtilsTest.sol
@@ -1,0 +1,342 @@
+// SPDX-FileCopyrightText: 2023 Lido <info@lido.fi>
+
+// SPDX-License-Identifier: GPL-3.0
+
+/* See contracts/COMPILERS.md */
+pragma solidity 0.8.9;
+
+
+import { MemUtils } from "../lib/MemUtils.sol";
+
+
+contract MemUtilsTest {
+    error BytesDontMatch(bytes actual, bytes expected);
+    error HashesDontMatch(bytes32 actual, bytes32 expected);
+    error AssertUintEqualFailed(string msg, uint256 actual, uint256 expected);
+
+    function getDataPtr(bytes memory arr) internal pure returns (uint256 dataPtr) {
+        assembly {
+            dataPtr := add(arr, 32)
+        }
+    }
+
+    function assertBytes(bytes memory actual, bytes memory expected) internal pure {
+        if (keccak256(actual) != keccak256(expected)) {
+            revert BytesDontMatch(actual, expected);
+        }
+    }
+
+    function assertUint(uint256 actual, uint256 expected, string memory msg) internal pure {
+        if (actual != expected) {
+            revert AssertUintEqualFailed(msg, actual, expected);
+        }
+    }
+
+    ///
+    /// memcpy
+    ///
+
+    function memcpy_copies_mem_chunks_that_are_multiples_of_32_bytes() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x3333333333333333333333333333333333333333333333333333333333333333),
+            bytes32(0x4444444444444444444444444444444444444444444444444444444444444444),
+            bytes32(0x5555555555555555555555555555555555555555555555555555555555555555)
+        );
+
+        MemUtils.memcpy(getDataPtr(src), getDataPtr(dst), 64);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222),
+            bytes32(0x5555555555555555555555555555555555555555555555555555555555555555)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_that_are_multiples_of_32_bytes_from_a_non_32b_offset() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222),
+            bytes32(0x3333333333333333333333333333333333333333333333333333333333333333)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x4444444444444444444444444444444444444444444444444444444444444444),
+            bytes32(0x5555555555555555555555555555555555555555555555555555555555555555),
+            bytes32(0x6666666666666666666666666666666666666666666666666666666666666666)
+        );
+
+        MemUtils.memcpy(getDataPtr(src) + 4, getDataPtr(dst), 64);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111122222222),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222233333333),
+            bytes32(0x6666666666666666666666666666666666666666666666666666666666666666)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_that_are_multiples_of_32b_to_a_non_32b_offset() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x3333333333333333333333333333333333333333333333333333333333333333),
+            bytes32(0x4444444444444444444444444444444444444444444444444444444444444444),
+            bytes32(0x5555555555555555555555555555555555555555555555555555555555555555)
+        );
+
+        MemUtils.memcpy(getDataPtr(src), getDataPtr(dst) + 4, 64);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x3333333311111111111111111111111111111111111111111111111111111111),
+            bytes32(0x1111111122222222222222222222222222222222222222222222222222222222),
+            bytes32(0x2222222255555555555555555555555555555555555555555555555555555555)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_that_are_multiples_of_32_bytes_from_and_to_a_non_32b_offset() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222),
+            bytes32(0x3333333333333333333333333333333333333333333333333333333333333333)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x4444444444444444444444444444444444444444444444444444444444444444),
+            bytes32(0x5555555555555555555555555555555555555555555555555555555555555555),
+            bytes32(0x6666666666666666666666666666666666666666666666666666666666666666)
+        );
+
+        MemUtils.memcpy(getDataPtr(src) + 4, getDataPtr(dst) + 3, 64);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x4444441111111111111111111111111111111111111111111111111111111122),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222233),
+            bytes32(0x3333336666666666666666666666666666666666666666666666666666666666)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_that_are_not_multiples_of_32_bytes() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x3333333333333333333333333333333333333333333333333333333333333333),
+            bytes32(0x4444444444444444444444444444444444444444444444444444444444444444)
+        );
+
+        MemUtils.memcpy(getDataPtr(src), getDataPtr(dst), 42);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222244444444444444444444444444444444444444444444)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_that_are_not_multiples_of_32_bytes_from_a_non_32b_offset() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x3333333333333333333333333333333333333333333333333333333333333333),
+            bytes32(0x4444444444444444444444444444444444444444444444444444444444444444)
+        );
+
+        MemUtils.memcpy(getDataPtr(src) + 3, getDataPtr(dst), 42);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111222222),
+            bytes32(0x2222222222222222222244444444444444444444444444444444444444444444)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_that_are_not_multiples_of_32_bytes_to_a_non_32b_offset() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x3333333333333333333333333333333333333333333333333333333333333333),
+            bytes32(0x4444444444444444444444444444444444444444444444444444444444444444)
+        );
+
+        MemUtils.memcpy(getDataPtr(src), getDataPtr(dst) + 3, 42);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x3333331111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x1111112222222222222222222244444444444444444444444444444444444444)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_that_are_not_multiples_of_32_bytes_from_and_to_a_non_32b_offset() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x3333333333333333333333333333333333333333333333333333333333333333),
+            bytes32(0x4444444444444444444444444444444444444444444444444444444444444444)
+        );
+
+        MemUtils.memcpy(getDataPtr(src) + 3, getDataPtr(dst) + 4, 42);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x3333333311111111111111111111111111111111111111111111111111111111),
+            bytes32(0x1122222222222222222222222222444444444444444444444444444444444444)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_shorter_than_32_bytes() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)
+        );
+
+        MemUtils.memcpy(getDataPtr(src), getDataPtr(dst), 5);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x1111111111222222222222222222222222222222222222222222222222222222)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_shorter_than_32_bytes_from_a_non_32b_offset() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0xcccccccccccccccccccccccccccccccccc8badf00d1234eeeeeeeeeeeeeeeeee)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)
+        );
+
+        MemUtils.memcpy(getDataPtr(src) + 17, getDataPtr(dst), 4);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x8badf00d22222222222222222222222222222222222222222222222222222222)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_shorter_than_32_bytes_to_a_non_32b_offset() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)
+        );
+
+        MemUtils.memcpy(getDataPtr(src), getDataPtr(dst) + 5, 5);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x2222222222111111111122222222222222222222222222222222222222222222)
+        ));
+    }
+
+    function memcpy_copies_mem_chunks_shorter_than_32_bytes_from_and_to_a_non_32b_offset() external pure {
+        bytes memory src = abi.encodePacked(
+            bytes32(0xcccccccccccccccccccccccccccccccccc8badf00d1234eeeeeeeeeeeeeeeeee)
+        );
+
+        bytes memory dst = abi.encodePacked(
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222)
+        );
+
+        MemUtils.memcpy(getDataPtr(src) + 17, getDataPtr(dst) + 3, 4);
+
+        assertBytes(dst, abi.encodePacked(
+            bytes32(0x2222228badf00d22222222222222222222222222222222222222222222222222)
+        ));
+    }
+
+    ///
+    /// keccakUint256Array
+    ///
+
+    function keccakUint256Array_calcs_keccak_over_a_uint_array() external pure {
+        uint256[] memory array = new uint256[](5);
+        array[0] = uint256(0x1111111111111111111111111111111111111111111111111111111111111111);
+        array[1] = uint256(0x2222222222222222222222222222222222222222222222222222222222222222);
+        array[2] = uint256(0x3333333333333333333333333333333333333333333333333333333333333333);
+        array[3] = uint256(0x4444444444444444444444444444444444444444444444444444444444444444);
+        array[4] = uint256(0x5555555555555555555555555555555555555555555555555555555555555555);
+
+        bytes32 expected = keccak256(abi.encodePacked(array));
+        bytes32 actual = MemUtils.keccakUint256Array(array);
+
+        if (actual != expected) {
+            revert HashesDontMatch(actual, expected);
+        }
+    }
+
+    function keccakUint256Array_calcs_keccak_over_an_empty_array() external pure {
+        uint256[] memory array = new uint256[](0);
+
+        bytes32 expected = keccak256(abi.encodePacked(array));
+        bytes32 actual = MemUtils.keccakUint256Array(array);
+
+        if (actual != expected) {
+            revert HashesDontMatch(actual, expected);
+        }
+    }
+
+    ///
+    /// trimUint256Array
+    ///
+
+    function trimUint256Array_decreases_length_of_a_uint_array() external pure {
+        uint256[] memory array = new uint256[](5);
+        array[0] = uint256(0x1111111111111111111111111111111111111111111111111111111111111111);
+        array[1] = uint256(0x2222222222222222222222222222222222222222222222222222222222222222);
+        array[2] = uint256(0x3333333333333333333333333333333333333333333333333333333333333333);
+        array[3] = uint256(0x4444444444444444444444444444444444444444444444444444444444444444);
+        array[4] = uint256(0x5555555555555555555555555555555555555555555555555555555555555555);
+
+        MemUtils.trimUint256Array(array, 2);
+
+        assertUint(array.length, 3, "array.length");
+        assertUint(array[0], uint256(0x1111111111111111111111111111111111111111111111111111111111111111), "array[0]");
+        assertUint(array[1], uint256(0x2222222222222222222222222222222222222222222222222222222222222222), "array[1]");
+        assertUint(array[2], uint256(0x3333333333333333333333333333333333333333333333333333333333333333), "array[2]");
+
+        assertBytes(abi.encodePacked(array), abi.encodePacked(
+            bytes32(0x1111111111111111111111111111111111111111111111111111111111111111),
+            bytes32(0x2222222222222222222222222222222222222222222222222222222222222222),
+            bytes32(0x3333333333333333333333333333333333333333333333333333333333333333)
+        ));
+    }
+
+    function trimUint256Array_allows_trimming_to_zero_length() external pure {
+        uint256[] memory array = new uint256[](3);
+        array[0] = uint256(0x1111111111111111111111111111111111111111111111111111111111111111);
+        array[1] = uint256(0x2222222222222222222222222222222222222222222222222222222222222222);
+        array[2] = uint256(0x3333333333333333333333333333333333333333333333333333333333333333);
+
+        MemUtils.trimUint256Array(array, 3);
+
+        assertUint(array.length, 0, "array.length");
+        assertBytes(abi.encodePacked(array), new bytes(0));
+    }
+
+    function trimUint256Array_reverts_on_trying_to_trim_by_more_than_length() external pure {
+        uint256[] memory array = new uint256[](3);
+        array[0] = uint256(0x1111111111111111111111111111111111111111111111111111111111111111);
+        array[1] = uint256(0x2222222222222222222222222222222222222222222222222222222222222222);
+        array[2] = uint256(0x3333333333333333333333333333333333333333333333333333333333333333);
+
+        MemUtils.trimUint256Array(array, 4);
+    }
+}

test/common/lib/mem-utils.test.js
@@ -1,0 +1,94 @@
+const { BN } = require('bn.js')
+const { assert } = require('chai')
+const { assertBn, assertEvent, assertAmountOfEvents } = require('@aragon/contract-helpers-test/src/asserts')
+const { assertRevert } = require('../../helpers/assertThrow')
+
+const MemUtilsTest = artifacts.require('MemUtilsTest')
+
+
+contract('AccountingOracle', ([deployer]) => {
+  let memUtilsTest
+
+  before(async () => {
+    memUtilsTest = await MemUtilsTest.new({from: deployer})
+  })
+
+  context('MemUtils.memcpy', () => {
+
+    it('copies mem chunks that are multiples of 32 bytes', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_that_are_multiples_of_32_bytes()
+    })
+
+    it('copies mem chunks that are multiples of 32 bytes from a non-32 byte offset', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_that_are_multiples_of_32_bytes_from_a_non_32b_offset()
+    })
+
+    it('copies mem chunks that are multiples of 32 bytes to a non-32 byte offset', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_that_are_multiples_of_32b_to_a_non_32b_offset()
+    })
+
+    it('copies mem chunks that are multiples of 32 bytes from and to a non-32 byte offset', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_that_are_multiples_of_32_bytes_from_and_to_a_non_32b_offset()
+    })
+
+    it('copies mem chunks that are not multiples of 32 bytes', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_that_are_not_multiples_of_32_bytes()
+    })
+
+    it('copies mem chunks that are not multiples of 32 bytes from a non-32 byte offset', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_that_are_not_multiples_of_32_bytes_from_a_non_32b_offset()
+    })
+
+    it('copies mem chunks that are not multiples of 32 bytes to a non-32 byte offset', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_that_are_not_multiples_of_32_bytes_to_a_non_32b_offset()
+    })
+
+    it('copies mem chunks that are not multiples of 32 bytes from and to a non-32 byte offset', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_that_are_not_multiples_of_32_bytes_from_and_to_a_non_32b_offset()
+    })
+
+    it('copies mem chunks shorher than 32 bytes', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_shorter_than_32_bytes()
+    })
+
+    it('copies mem chunks shorher than 32 bytes from a non-32 byte offset', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_shorter_than_32_bytes_from_a_non_32b_offset()
+    })
+
+    it('copies mem chunks shorher than 32 bytes to a non-32 byte offset', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_shorter_than_32_bytes_to_a_non_32b_offset()
+    })
+
+    it('copies mem chunks shorher than 32 bytes from and to a non-32 byte offset', async () => {
+      await memUtilsTest.memcpy_copies_mem_chunks_shorter_than_32_bytes_from_and_to_a_non_32b_offset()
+    })
+  })
+
+  context('MemUtils.keccakUint256Array', () => {
+
+    it('calculates a keccak256 over a uint256 array', async () => {
+      await memUtilsTest.keccakUint256Array_calcs_keccak_over_a_uint_array()
+    })
+
+    it('calculates a keccak256 over an empty uint256 array', async () => {
+      await memUtilsTest.keccakUint256Array_calcs_keccak_over_an_empty_array()
+    })
+  })
+
+  context('MemUtils.trimUint256Array', () => {
+
+    it('decreases length of a uint256 array', async () => {
+      await memUtilsTest.trimUint256Array_decreases_length_of_a_uint_array()
+    })
+
+    it('allows trimming to a zero length', async () => {
+      await memUtilsTest.trimUint256Array_allows_trimming_to_zero_length()
+    })
+
+    it('reverts on trying to trim by more than the array length', async () => {
+      await assertRevert(
+        memUtilsTest.trimUint256Array_reverts_on_trying_to_trim_by_more_than_length()
+      )
+    })
+  })
+})