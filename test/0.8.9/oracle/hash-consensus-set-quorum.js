1780f4bb1ad50363efe60309224b68da6a85ac42
Evgeny Taktarov <je.mouth@gmail.com>
2023-02-07 20:49:48 +0700

    feat: setQuorum advanced tests

--- test/0.8.9/oracle/hash-consensus-set-quorum.js
+++ test/0.8.9/oracle/hash-consensus-set-quorum.js
@@ -5,17 +5,19 @@ const { assertRevert } = require('../../helpers/assertThrow')
 const {
   SLOTS_PER_EPOCH,
   SECONDS_PER_SLOT,
-  SLOTS_PER_FRAME,
+  SECONDS_PER_EPOCH,
+  SECONDS_PER_FRAME,
   computeEpochFirstSlot,
   computeTimestampAtEpoch,
   ZERO_HASH,
   HASH_1,
+  HASH_3,
   CONSENSUS_VERSION,
   deployHashConsensus
 } = require('./hash-consensus-deploy.test')
 
 contract('HashConsensus', ([admin, member1, member2, member3]) => {
-  context('setQuorum', () => {
+  context('setQuorum changes getQuorum', () => {
     let consensus
 
     beforeEach(async () => {
@@ -67,6 +69,88 @@ contract('HashConsensus', ([admin, member1, member2, member3]) => {
 
       await consensus.setQuorum(2)
       assert.equal(+(await consensus.getQuorum()), 2)
+    })
+  })
+
+  context('setQuorum changes the effective quorum', () => {
+    let consensus
+    let frame
+
+    beforeEach(async () => {
+      const deployed = await deployHashConsensus(admin, { initialEpoch: 1 })
+      consensus = deployed.consensus
+
+      await consensus.addMember(member1, 1, { from: admin })
+      await consensus.addMember(member2, 2, { from: admin })
+      await consensus.addMember(member3, 3, { from: admin })
+      frame = await consensus.getCurrentFrame()
+    })
+
+    it('setQuorum increases and cancels current consensus', async () => {
+      await consensus.setQuorum(2)
+
+      const tx1 = await consensus.submitReport(frame.refSlot, HASH_1, CONSENSUS_VERSION, { from: member1 })
+      assertEvent(tx1, 'ReportReceived', { expectedArgs: { refSlot: frame.refSlot, member: member1, report: HASH_1 } })
+      assertAmountOfEvents(tx1, 'ConsensusReached', { expectedAmount: 0 })
+
+      const tx2 = await consensus.submitReport(frame.refSlot, HASH_1, CONSENSUS_VERSION, { from: member2 })
+      assertEvent(tx2, 'ReportReceived', {
+        expectedArgs: { refSlot: frame.refSlot, member: member2, report: HASH_1 }
+      })
+      assertAmountOfEvents(tx2, 'ConsensusReached', { expectedAmount: 1 })
+
+      const tx3 = await consensus.setQuorum(3)
+      assertAmountOfEvents(tx3, 'ConsensusReached', { expectedAmount: 0 })
+      const consensusState = await consensus.getConsensusState()
+      assert.equal(consensusState.consensusReport, ZERO_HASH)
+      assert.isFalse(consensusState.isReportProcessing)
+    })
+
+    it('setQuorum decreases, triggers quorum once and goes up/down again', async () => {
+      await consensus.setQuorum(3)
+
+      const tx1 = await consensus.submitReport(frame.refSlot, HASH_1, CONSENSUS_VERSION, { from: member1 })
+      assertEvent(tx1, 'ReportReceived', { expectedArgs: { refSlot: frame.refSlot, member: member1, report: HASH_1 } })
+      assertAmountOfEvents(tx1, 'ConsensusReached', { expectedAmount: 0 })
+
+      const tx2 = await consensus.submitReport(frame.refSlot, HASH_1, CONSENSUS_VERSION, { from: member2 })
+      assertEvent(tx2, 'ReportReceived', {
+        expectedArgs: { refSlot: frame.refSlot, member: member2, report: HASH_1 }
+      })
+      assertAmountOfEvents(tx2, 'ConsensusReached', { expectedAmount: 0 })
+
+      const tx3 = await consensus.setQuorum(2)
+      assertAmountOfEvents(tx3, 'ConsensusReached', { expectedAmount: 1 })
+      let consensusState = await consensus.getConsensusState()
+      assert.equal(consensusState.consensusReport, HASH_1)
+
+      const tx4 = await consensus.setQuorum(3)
+      assertAmountOfEvents(tx4, 'ConsensusReached', { expectedAmount: 0 })
+      consensusState = await consensus.getConsensusState()
+      assert.equal(consensusState.consensusReport, ZERO_HASH)
+
+      const tx5 = await consensus.setQuorum(2)
+      assertAmountOfEvents(tx5, 'ConsensusReached', { expectedAmount: 0 })
+    })
+
+    it('setQuorum does not re-trigger consensus if hash is already being processed', async () => {
+      await consensus.setQuorum(3)
+      const tx1 = await consensus.submitReport(frame.refSlot, HASH_1, CONSENSUS_VERSION, { from: member1 })
+      assertEvent(tx1, 'ReportReceived', {
+        expectedArgs: { refSlot: frame.refSlot, member: member1, report: HASH_1 }
+      })
+      assertAmountOfEvents(tx1, 'ConsensusReached', { expectedAmount: 0 })
+
+      const tx2 = await consensus.submitReport(frame.refSlot, HASH_1, CONSENSUS_VERSION, { from: member2 })
+      assertEvent(tx2, 'ReportReceived', {
+        expectedArgs: { refSlot: frame.refSlot, member: member2, report: HASH_1 }
+      })
+      assertAmountOfEvents(tx2, 'ConsensusReached', { expectedAmount: 0 })
+
+      await consensus.advanceTimeBy(SECONDS_PER_FRAME)
+
+      const tx3 = await consensus.setQuorum(2)
+      assertAmountOfEvents(tx3, 'ConsensusReached', { expectedAmount: 0 })
     })
   })
 })
