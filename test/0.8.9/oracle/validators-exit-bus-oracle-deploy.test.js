a501c1abd93106e91dc06d31154785663c017dfd
Sam Kozin <sam.kozin@gmail.com>
2023-02-08 19:47:46 +0100

    oracles: revive tests broken by 387c94a

--- test/0.8.9/oracle/validators-exit-bus-oracle-deploy.test.js
+++ test/0.8.9/oracle/validators-exit-bus-oracle-deploy.test.js
@@ -80,6 +80,7 @@ async function deployOracleReportSanityCheckerForExitBus(lidoLocator, admin) {
 
 async function deployExitBusOracle(admin, {
   dataSubmitter = null,
+  lastProcessingRefSlot = 0,
   resumeAfterDeploy = false,
 } = {}) {
   const locator = await deployLocatorWithInvalidImplementation(admin)
@@ -98,13 +99,11 @@ async function deployExitBusOracle(admin, {
     oracleReportSanityChecker : oracleReportSanityChecker,
   })
 
-  const lastProcessedRefSlot = 0
-
   const tx = await oracle.initialize(
     admin,
     consensus.address,
     CONSENSUS_VERSION,
-    lastProcessedRefSlot,
+    lastProcessingRefSlot,
     {from: admin}
   )
 
