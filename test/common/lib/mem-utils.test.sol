2c083fc9a73e964f9a6110267205ff0676c471e1
Bogdan Kovtun <psirex48@gmail.com>
2023-02-16 01:51:32 +0400

    Add Foundry tests for MinFirstAllocationStrategy

--- test/common/lib/mem-utils.test.sol
+++ test/common/lib/mem-utils.test.sol
@@ -1,16 +1,17 @@
+// SPDX-FileCopyrightText: 2023 Lido <info@lido.fi>
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity 0.8.9;
 
-import { MemUtils } from "contracts/common/lib/MemUtils.sol";
 import "forge-std/Test.sol";
+import { MemUtils } from "contracts/common/lib/MemUtils.sol";
 
 contract MemUtilsTestFoundry is Test {
-    uint256 testNumber;
+    ///
+    /// unsafeAllocateBytes
+    ///
 
-    function setUp() public {
-        testNumber = 42;
-    }
-
-    function testUnsafeAlloc_allocates_empty_byte_array() external {
+    function test_unsafeAlloc_allocates_empty_byte_array() external {
         // disable all compiler optimizations by including an assembly block not marked as mem-safe
         assembly {
             mstore(0x00, 0x1)
