40f8eadb4507bb676ca13cde4ee44db1af6f005d
Artyom Veremeenko <arwer13@gmail.com>
2023-02-15 14:45:57 +0400

    fix dao-{mainnetfork,zhejiang}-deploy.sh files

--- dao-mainnetfork-deploy.sh
+++ dao-mainnetfork-deploy.sh
@@ -4,6 +4,6 @@ set -o pipefail
 
 # first local account by default
 export DEPLOYER=0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1
-export NETWORK=${NETWORK:=mainnetfork}
+export NETWORK=mainnetfork
 
 bash dao-deploy.sh
--- dao-zhejiang-deploy.sh
+++ dao-zhejiang-deploy.sh
@@ -2,8 +2,7 @@
 set -e +u
 set -o pipefail
 
-# first local account by default
 export DEPLOYER=0xa5F1d7D49F581136Cf6e58B32cBE9a2039C48bA1
-export NETWORK=${NETWORK:=zhejiang}
+export NETWORK=zhejiang
 
 bash dao-deploy.sh
