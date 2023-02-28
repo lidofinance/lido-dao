1d856f573fe1e8076e5ebbbdd1c44dbd94369471
Alexey Potapkin <folkyatina@gmail.com>
2023-02-28 15:55:43 +0200

    test: add test 2 requests 2 batches

--- test/0.8.9/withdrawal-queue-share-rate-changes.test.js
+++ test/0.8.9/withdrawal-queue-share-rate-changes.test.js
@@ -41,7 +41,7 @@ contract('WithdrawalQueue', ([owner, daoAgent, finalizer, user]) => {
     await snapshot()
   })
 
-  context(`multiple requests with diff entry share rate`, async () => {
+  context(`2 requests with diff share rate, maxShareRate == shareRate(1)`, async () => {
     ///
     /// invariant 1: all requests in the same batch should be finalized using the same share rate
     ///
@@ -104,6 +104,75 @@ contract('WithdrawalQueue', ([owner, daoAgent, finalizer, user]) => {
 
     it(`second request is fullfilled with 1 ETH`, async () => {
       assert.isClose(claimableEther[1], e18(1), 10)
+    })
+  })
+
+  context(`2 requests 2 batches, shareRate(2) > maxShareRate > shareRate(1)`, async () => {
+    ///
+    /// invariant 1: all requests in the same batch should be finalized using the same share rate
+    ///
+    /// invariant 2: a withdrawal request cannot be finalized using a lower share rate than the
+    /// minimum share rate that was reported by the oracle since the last oracle report before
+    /// the request was added to the queue
+    ///
+    after(rollback)
+
+    const requestIds = [0, 0]
+
+    it(`share rate 1.0: a user requests a withdrawal of 1 stETH (10**18 shares)`, async () => {
+      const tx = await queue.requestWithdrawals([e18(1)], user, { from: user })
+      requestIds[0] = +getFirstEventArgs(tx, 'WithdrawalRequested').requestId
+      assert.equals(await queue.unfinalizedStETH(), e18(1))
+    })
+
+    it(`protocol receives rewards, changing share rate to 2.0`, async () => {
+      await queue.onPreRebase()
+      await setShareRate(2)
+    })
+
+    it(`share rate 2.0: a user requests a withdrawal of 2 stETH (10**18 shares)`, async () => {
+      const tx = await queue.requestWithdrawals([e18(2)], user, { from: user })
+      requestIds[1] = +getFirstEventArgs(tx, 'WithdrawalRequested').requestId
+      assert.equals(await queue.unfinalizedStETH(), e18(3))
+    })
+
+    it(`protocol receives slashing, changing share rate to 1.0`, async () => {
+      await queue.onPreRebase()
+      await setShareRate(1)
+    })
+
+    let batches
+    const maxShareRate = e27(1.5)
+
+    it(`both requests can be finalized with 2 ETH`, async () => {
+      const result = await queue.calculateFinalizationBatches(maxShareRate, MAX_UINT256, [e18(2.5), false, []])
+      assert.isTrue(result.finished)
+
+      console.log(result)
+
+      const batch = await queue.prefinalize.call(result.batches, maxShareRate)
+      assert.equals(batch.ethToLock, e18(2.5))
+      assert.equals(batch.sharesToBurn, e18(2))
+
+      batches = result.batches
+    })
+
+    let claimableEther
+
+    it(`requests get finalized`, async () => {
+      await queue.finalize(batches, maxShareRate, { from: finalizer, value: e18(2.5) })
+      assert.equals(await queue.getLastFinalizedRequestId(), requestIds[1])
+
+      const hints = await queue.findCheckpointHints(requestIds, 1, await queue.getLastCheckpointIndex())
+      claimableEther = await queue.getClaimableEther(requestIds, hints)
+    })
+
+    it(`first request is fullfilled with 1 ETH`, async () => {
+      assert.isClose(claimableEther[0], e18(1), 10)
+    })
+
+    it(`second request is fullfilled with 1 ETH`, async () => {
+      assert.isClose(claimableEther[1], e18(1.5), 10)
     })
   })
 })
