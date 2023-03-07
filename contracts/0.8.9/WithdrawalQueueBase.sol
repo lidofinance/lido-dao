e3f1319384a86b6062570036ca922a7d1b905d3f
Alexey Potapkin <folkyatina@gmail.com>
2023-03-07 11:47:52 +0200

    📚: more docs for wq

--- contracts/0.8.9/WithdrawalQueueBase.sol
+++ contracts/0.8.9/WithdrawalQueueBase.sol
@@ -149,20 +149,33 @@ abstract contract WithdrawalQueueBase {
     }
 
     //
-    // FINALIZATION
+    // FINALIZATION FLOW
     //
     // Process when protocol is fixing the withdrawal request value and lock the required amount of ETH.
-    // Locked ETH can be claimed by the owner of the request
-    // Finalization is a part of the oracle report
-    // Steps:
+    // The value of a request after finalization can be:
+    //  - nominal (when the amount of eth locked for this request are equal to the request's stETH)
+    //  - discounted (when the amount of eth will be lower, because the protocol share rate dropped
+    //   before request is finalized, so it will be equal to `request's shares` * `protocol share rate`)
+    // The parameters that are required for finalization are:
+    //  - current share rate of the protocol
+    //  - id of the last request that can be finalized
+    //  - the amount of eth that must be locked for these requests
+    // To calculate the eth amount we'll need to know which requests int the queue will be finalized as nominal
+    // and which as discounted and the exact value of the discount. It's impossible to calculate without the unbounded
+    // loop over the unfinalized part of the queue. So, we need to extract a part of the algorithm off-chain, bring the
+    // result with oracle report and check it later and check the resukt later.
+    // So, we came to this solution:
     // Off-chain
-    // 1. Oracle daemon precalculates finalization batches' boundaries using `calculateFinalizationBatches()`
-    // On-chain
-    // 2. AccountingOracle contract invokes `onOracleReport()` handler to update last report timestamp for the queue
-    // 3. Lido contract, during the report handling, calculates the value of finalization batch in eth and shares
-    //  and checks its correctness using `prefinalize()`
-    // 4. Lido contract finalize the batch of requests passing the required ether along with `finalize()` method
-    //
+    // 1. Oracle iterates over the queue off-chain and calculate the id of the latest finalizable request
+    // in the queue. Then it splits all the requests that will be finalized into batches the way,
+    // that requests in a batch are all nominal or all discounted.
+    // And passes them in the report as the array of the ending ids of these batches. So it can be reconstructed like
+    // `[lastFinalizedRequestId+1, batches[0]], [batches[0]+1, batches[1]] ... [batches[n-2], batches[n-1]]`
+    // 2. Contract checks the validity of the batches on-chain and calculate the amount of eth required to
+    //  finalize them. It can be done without unbounded loop using partial sums that are calculated on request enqueueing.
+    // 3. Contract marks the request's as finalized and locks the eth for claiming. It also,
+    //  set's the discount checkpoint for these request's if required that will be applied on claim for each request's
+    // individually depending on request's share rate.
 
     /// @notice transient state that is used to pass intemediate results between several `calculateFinalizationBatches`
     //   invokations
@@ -530,7 +543,7 @@ abstract contract WithdrawalQueueBase {
         if (!success) revert CantSendValueRecipientMayHaveReverted();
     }
 
-    /// @dev calculate batch stats for the batch of `(_preStartRequest, _endRequest]`
+    /// @dev calculate batch stats (shareRate, stETH and shares) for the batch of `(_preStartRequest, _endRequest]`
     function _calcBatch(
         WithdrawalRequest memory _preStartRequest,
         WithdrawalRequest memory _endRequest
