989d95bc65928b334f1c16706445c9496207c369
Azat Serikov <azatsdev@gmail.com>
2023-03-21 18:59:11 +0600

    feat: update NOR frontend

--- apps/node-operators-registry/app/src/script.js
+++ apps/node-operators-registry/app/src/script.js
@@ -4,6 +4,97 @@ import Aragon, { events } from '@aragon/api'
 
 const app = new Aragon()
 
+const createFetcher =
+  (functionName, ...args) =>
+  () =>
+    app.call(functionName, ...args).toPromise()
+
+const offset = 0
+const MAX_OPERATORS = 200
+const getNodeOperatorsIds = createFetcher(
+  'getNodeOperatorIds',
+  offset,
+  MAX_OPERATORS
+)
+
+const getNodeOperator = (nodeOperatorId) =>
+  createFetcher('getNodeOperator', nodeOperatorId, true)()
+
+const getNodeOperators = async () => {
+  const nodeOperatorsIds = await getNodeOperatorsIds()
+  const promises = nodeOperatorsIds.map((id) => getNodeOperator(id))
+  const settledPromises = await Promise.allSettled(promises)
+  const nodeOperators = settledPromises.map((settled) => settled.value)
+  return nodeOperators.map((no, i) => ({ ...no, id: nodeOperatorsIds[i] }))
+}
+
+const protocolVariables = [
+  {
+    stateKey: 'stakingModuleSummary',
+    updateEvents: [
+      'VettedSigningKeysCountChanged',
+      'DepositedSigningKeysCountChanged',
+      'ExitedSigningKeysCountChanged',
+      'TotalSigningKeysCountChanged',
+      'StuckValidatorsCountChanged',
+      'RefundedValidatorsCountChanged',
+    ],
+    fetch: createFetcher('getStakingModuleSummary'),
+  },
+  {
+    stateKey: 'nonce',
+    updateEvents: ['KeysOpIndexSet', 'NonceChanged'],
+    fetch: createFetcher('getNonce'),
+  },
+  {
+    stateKey: 'nodeOperatorsCount',
+    updateEvents: [],
+    fetch: createFetcher('getNodeOperatorsCount'),
+  },
+  {
+    stateKey: 'activeNodeOperatorsCount',
+    updateEvents: [],
+    fetch: createFetcher('getActiveNodeOperatorsCount'),
+  },
+  {
+    stateKey: 'stuckPenaltyDelay',
+    updateEvents: [],
+    fetch: createFetcher('getStuckPenaltyDelay'),
+  },
+  {
+    stateKey: 'nodeOperators',
+    updateEvents: [
+      'NodeOperatorAdded',
+      'NodeOperatorActiveSet',
+      'NodeOperatorNameSet',
+      'NodeOperatorRewardAddressSet',
+      'NodeOperatorTotalKeysTrimmed',
+      '',
+    ],
+    fetch: getNodeOperators,
+  },
+  {
+    stateKey: 'stakingModuleType',
+    updateEvents: ['StakingModuleTypeSet'],
+    fetch: createFetcher('getType'),
+  },
+  {
+    stateKey: 'hasInitialized',
+    updateEvents: [],
+    fetch: createFetcher('hasInitialized'),
+  },
+  {
+    stateKey: 'initializationBlock',
+    updateEvents: [],
+    fetch: createFetcher('getInitializationBlock'),
+  },
+  {
+    stateKey: 'contractVersion',
+    updateEvents: ['ContractVersionSet'],
+    fetch: createFetcher('getContractVersion'),
+  },
+]
+
 app.store(
   async (state, { event }) => {
     const nextState = {
@@ -11,39 +102,26 @@ app.store(
     }
 
     try {
-      switch (event) {
-        case 'NodeOperatorAdded':
-          // eslint-disable-next-line no-case-declarations
-          const nodeOperatorsCount = await getNodeOperatorsCount()
-          return {
-            ...nextState,
-            nodeOperatorsCount,
-            activeNodeOperatorsCount: await getActiveNodeOperatorsCount(),
-            nodeOperators: await getNodeOperators(nodeOperatorsCount),
-          }
-        case 'NodeOperatorActiveSet':
-          return {
-            ...nextState,
-            activeNodeOperatorsCount: await getActiveNodeOperatorsCount(),
-            nodeOperators: await getNodeOperators(nextState.nodeOperatorsCount),
-          }
-        case 'SigningKeyAdded':
-          return {
-            ...nextState,
-            nodeOperators: await getNodeOperators(nextState.nodeOperatorsCount),
-          }
-        case 'NodeOperatorStakingLimitSet':
-          return {
-            ...nextState,
-            nodeOperators: await getNodeOperators(nextState.nodeOperatorsCount),
-          }
-        case events.SYNC_STATUS_SYNCING:
-          return { ...nextState, isSyncing: true }
-        case events.SYNC_STATUS_SYNCED:
-          return { ...nextState, isSyncing: false }
-        default:
-          return state
+      if (event === events.SYNC_STATUS_SYNCING) {
+        return { ...nextState, isSyncing: true }
       }
+
+      if (event === events.SYNC_STATUS_SYNCED) {
+        return { ...nextState, isSyncing: false }
+      }
+
+      const variable = protocolVariables.find(({ updateEvents }) =>
+        updateEvents.includes(event)
+      )
+
+      if (variable) {
+        return {
+          ...nextState,
+          [variable.stateKey]: await variable.fetch(),
+        }
+      }
+
+      return nextState
     } catch (err) {
       console.log(err)
     }
@@ -54,43 +132,45 @@ app.store(
 )
 
 /***********************
+ *                     *
  *   Event Handlers    *
+ *                     *
  ***********************/
 
 function initializeState() {
   return async (cachedState) => {
-    const nodeOperatorsCount = await getNodeOperatorsCount()
+    const promises = protocolVariables.map((v) => v.fetch())
+
+    const settledPromises = await Promise.allSettled(promises)
+
+    const updatedState = settledPromises.reduce((stateObject, cur, index) => {
+      stateObject[protocolVariables[index].stateKey] = cur.value
+      return stateObject
+    }, {})
+
     return {
       ...cachedState,
-      nodeOperatorsCount,
-      activeNodeOperatorsCount: await getActiveNodeOperatorsCount(),
-      nodeOperators: await getNodeOperators(nodeOperatorsCount),
+      ...updatedState,
     }
   }
 }
 
-async function getNodeOperatorsCount() {
-  return await app.call('getNodeOperatorsCount').toPromise()
-}
-
-async function getActiveNodeOperatorsCount() {
-  return await app.call('getActiveNodeOperatorsCount').toPromise()
-}
-
-function getNodeOperatorInfo(index) {
-  return app.call('getNodeOperator', index, true).toPromise()
-}
-
-async function getNodeOperators(numberOfOperators) {
-  const nodeOperators = []
-
-  for (let id = 0; id < numberOfOperators; id++) {
-    const info = await getNodeOperatorInfo(id)
-    nodeOperators.push({
-      ...info,
-      id,
-    })
-  }
-
-  return nodeOperators
-}
+// addNodeOperator
+// activateNodeOperator
+// deactivateNodeOperator
+// setNodeOperatorName
+// setNodeOperatorRewardAddress
+// setNodeOperatorStakingLimit
+// updateStuckValidatorsCount
+// updateExitedValidatorsCount
+// updateRefundedValidatorsCount
+// unsafeUpdateValidatorsCount
+// updateTargetValidatorsLimits
+// invalidateReadyToDepositKeysRange
+// obtainDepositData
+// addSigningKeys
+// addSigningKeysOperatorBH
+// removeSigningKey
+// removeSigningKeyOperatorBH
+// removeSigningKeysOperatorBH
+// setStuckPenaltyDelay
