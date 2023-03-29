fe2d276d149eb185583b853d69a89fe9aa1c251e
Azat Serikov <azatsdev@gmail.com>
2023-03-29 14:11:40 +0600

    feat: bg script

--- apps/legacyoracle/app/src/script.js
+++ apps/legacyoracle/app/src/script.js
@@ -1,6 +1,9 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import Aragon, { events } from '@aragon/api'
+import LidoABI from "./abi/Lido.abi.json"
+import LidoLocatorABI from "./abi/LidoLocator.abi.json"
+import AccountingOracleABI from "./abi/AccountingOracle.abi.json"
 
 const app = new Aragon()
 
@@ -11,23 +14,15 @@ app.store(
     }
 
     try {
-      switch (event) {
-        case 'ExpectedEpochIdUpdated':
-          return { ...nextState, expectedEpochId: await getExpectedEpochId() }
-        case 'UI:UpdateFrame':
-          return { ...nextState, currentFrame: await getCurrentFrame() }
-        case 'ContractVersionSet':
-          return {
-            ...nextState,
-            version: await getVersion(),
-          }
-        case events.SYNC_STATUS_SYNCING:
-          return { ...nextState, isSyncing: true }
-        case events.SYNC_STATUS_SYNCED:
-          return { ...nextState, isSyncing: false }
-        default:
-          return state
+      if (event === events.SYNC_STATUS_SYNCING) {
+        return { ...nextState, isSyncing: true }
       }
+
+      if (event === events.SYNC_STATUS_SYNCED) {
+        return { ...nextState, isSyncing: false }
+      }
+
+      return nextState
     } catch (err) {
       console.log(err)
     }
@@ -43,40 +38,95 @@ app.store(
  *                     *
  ***********************/
 
+const accountingOracleState = [
+  {
+    appStateKey: "extraDataFormatEmpty",
+    contractFunction: "EXTRA_DATA_FORMAT_EMPTY"
+  },
+  {
+    appStateKey: "extraDataFormatList",
+    contractFunction: "EXTRA_DATA_FORMAT_LIST"
+  },
+  {
+    appStateKey: "extraDataTypeExitedValidators",
+    contractFunction: "EXTRA_DATA_TYPE_EXITED_VALIDATORS"
+  },
+  {
+    appStateKey: "extraDataTypeStuckValidators",
+    contractFunction: "EXTRA_DATA_TYPE_STUCK_VALIDATORS"
+  },
+  {
+    appStateKey: "genesisTime",
+    contractFunction: "GENESIS_TIME"
+  },
+  {
+    appStateKey: "legacyOracle",
+    contractFunction: "LEGACY_ORACLE"
+  },
+  {
+    appStateKey: "secondsPerSlot",
+    contractFunction: "SECONDS_PER_SLOT"
+  },
+  {
+    appStateKey: "consensusContract",
+    contractFunction: "getConsensusContract"
+  },
+  {
+    appStateKey: "consensusReport",
+    contractFunction: "getConsensusReport"
+  },
+  {
+    appStateKey: "consensusVersion",
+    contractFunction: "getConsensusVersion"
+  },
+  {
+    appStateKey: "contractVersion",
+    contractFunction: "getContractVersion"
+  },
+  {
+    appStateKey: "lastProcessingRefSlot",
+    contractFunction: "getLastProcessingRefSlot"
+  },
+  {
+    appStateKey: "processingState",
+    contractFunction: "getProcessingState"
+  },
+  {
+    appStateKey: "contractVersion",
+    contractFunction: "getContractVersion"
+  },
+  {
+    appStateKey: "contractVersion",
+    contractFunction: "getContractVersion"
+  },
+]
+
 function initializeState() {
   return async (cachedState) => {
-    const [
-      currentFrame,
-      lastCompletedReportDelta,
-      version,
-    ] = await Promise.all([
-      getCurrentFrame(),
-      getLastCompletedReportDelta(),
-      getVersion(),
-    ])
+
+    console.log("HELLO")
+    const lido = await app.call("getLido").toPromise()
+    const lidoContract = app.external(lido, LidoABI)
+
+
+    const locator = await lidoContract.getLidoLocator().toPromise()
+    const locatorContract = app.external(locator, LidoLocatorABI)
+
+
+    const accountingOracle = await locatorContract.accountingOracle().toPromise()
+    const accountingOracleContract = app.external(accountingOracle, AccountingOracleABI)
+    const aoPromises = accountingOracleState.map(({ contractFunction }) => accountingOracleContract[contractFunction]().toPromise())
+    const settledAoPromises = await Promise.allSettled(aoPromises)
+    const aoState = settledAoPromises.reduce((stateObject, cur, index) => {
+      stateObject[accountingOracleState[index].appStateKey] = cur.value
+      return stateObject
+    }, {})
+
 
     return {
       ...cachedState,
-      currentFrame,
-      lastCompletedReportDelta,
-      version,
+      lido,
+      ...aoState
     }
   }
-}
-
-async function getCurrentFrame() {
-  const frame = await app.call('getCurrentFrame').toPromise()
-  return {
-    frameEpochId: String(frame.frameEpochId),
-    frameStartTime: +frame.frameStartTime,
-    frameEndTime: +frame.frameEndTime,
-  }
-}
-
-function getLastCompletedReportDelta() {
-  return app.call('getLastCompletedReportDelta').toPromise()
-}
-
-async function getVersion() {
-  return app.call('getVersion').toPromise()
 }
