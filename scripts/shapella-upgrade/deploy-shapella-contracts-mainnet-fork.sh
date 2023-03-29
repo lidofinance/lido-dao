4163a50fdab70dbd43c2aa046e9c3cec056c9c8b
Artyom Veremeenko <arwer13@gmail.com>
2023-03-29 10:47:05 +0400

    update shapella upgrade deploy scripts and parameters for mainnet

--- scripts/shapella-upgrade/deploy-shapella-contracts-mainnet-fork.sh
+++ scripts/shapella-upgrade/deploy-shapella-contracts-mainnet-fork.sh
@@ -3,5 +3,6 @@
 export DEPLOYER=0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1
 export NETWORK=mainnet-fork-shapella-upgrade
 # export GAS_PRICE=100000000000
+export DEFAULT_CONFIG_FILE="deployed-mainnet-upgrade-defaults.json"
 
 bash scripts/shapella-upgrade/deploy-shapella-contracts.sh
