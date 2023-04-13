30b9e9b5ec8ad5afb52e7e2fb72279ddb4c8fadd
Artyom Veremeenko <arwer13@gmail.com>
2023-04-13 14:17:48 +0400

    fix(deploy-shapella-contracts-mainnet-fork.sh): set 0 gas price

--- scripts/shapella-upgrade/deploy-shapella-contracts-mainnet-fork.sh
+++ scripts/shapella-upgrade/deploy-shapella-contracts-mainnet-fork.sh
@@ -21,7 +21,7 @@ fork_pid=$$
 
 export DEPLOYER=0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1 # ganache account 0 (private key should be added to accounts.json)
 export NETWORK=mainnet-fork-shapella-upgrade
-# export GAS_PRICE=100000000000
+export GAS_PRICE=0
 export DEFAULT_CONFIG_FILE="deployed-mainnet-upgrade-defaults.json"
 
 sleep 15
