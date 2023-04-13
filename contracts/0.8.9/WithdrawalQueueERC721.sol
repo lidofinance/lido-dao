19f19b53142bea551aefc20e1e22361f7745862c
Alexey Potapkin <folkyatina@gmail.com>
2023-04-13 13:54:29 +0300

    fix: wrong fromId in BatchMetadataUpdate event

--- contracts/0.8.9/WithdrawalQueueERC721.sol
+++ contracts/0.8.9/WithdrawalQueueERC721.sol
@@ -150,11 +150,13 @@ contract WithdrawalQueueERC721 is IERC721Metadata, IERC4906, WithdrawalQueue {
         _checkResumed();
         _checkRole(FINALIZE_ROLE, msg.sender);
 
+        uint256 firstFinalizedRequestId = getLastFinalizedRequestId() + 1;
+
         _finalize(_lastRequestIdToBeFinalized, msg.value, _maxShareRate);
 
         // ERC4906 metadata update event
         // We are updating all unfinalized to make it look different as they move closer to finalization in the future
-        emit BatchMetadataUpdate(getLastFinalizedRequestId() + 1, getLastRequestId());
+        emit BatchMetadataUpdate(firstFinalizedRequestId, getLastRequestId());
     }
 
     /// @dev See {IERC721-balanceOf}.
