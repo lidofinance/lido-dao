bbc67a703af5065d1cab4e9eb071ae46ae9fa817
Alexandr Tarelkin <alexandrtarelkin92@gmail.com>
2023-04-19 10:48:07 +0300

    scripts: kill old fork

--- scripts/shapella-upgrade/deploy-shapella-contracts-mainnet-fork.sh
+++ scripts/shapella-upgrade/deploy-shapella-contracts-mainnet-fork.sh
@@ -3,19 +3,21 @@
 # trap ctrl-c and call ctrl_c()
 trap ctrl_c INT
 
+local_rpc_port=7777
 fork_pid=0
 
 function ctrl_c() {
     if [[ $fork_pid > 0 ]]
     then
         echo "Stopping ganache"
-        kill -9 $fork_pid
+        kill -15 $fork_pid
     fi
     exit 0
 }
 
 # Run ganache from the scripts at first with
-fork_command="npx ganache --chain.vmErrorsOnRPCResponse true --wallet.totalAccounts 10 --chain.chainId 1337 --fork.url https://mainnet.infura.io/v3/$WEB3_INFURA_PROJECT_ID --miner.blockGasLimit 92000000  --server.port 7777 --hardfork istanbul -d"
+(nc -vz 127.0.0.1 $local_rpc_port) &>/dev/null && kill -15 $(lsof -t -i:$local_rpc_port)
+fork_command="npx ganache --chain.vmErrorsOnRPCResponse true --wallet.totalAccounts 10 --chain.chainId 1337 --fork.url https://mainnet.infura.io/v3/$WEB3_INFURA_PROJECT_ID --miner.blockGasLimit 92000000  --server.port $local_rpc_port --hardfork istanbul -d"
 $fork_command &
 fork_pid=$$
 
@@ -25,7 +27,7 @@ export GAS_PRICE=0
 export DEFAULT_CONFIG_FILE="deployed-mainnet-upgrade-defaults.json"
 export LIDO_LOCATOR_PROXY_PREDEPLOYED="0xC1d0b3DE6792Bf6b4b37EccdcC24e45978Cfd2Eb"
 
-sleep 15
+sleep 10
 
 bash scripts/shapella-upgrade/deploy-shapella-contracts.sh
 
