74effae12ed120b0c9723eca5894247a68e98ce3
Eugene Mamin <TheDZhon@gmail.com>
2023-05-01 21:17:32 +0300

    fix: cd to script dir on start

--- bytecode-verificator/bytecode_verificator.sh
+++ bytecode-verificator/bytecode_verificator.sh
@@ -60,7 +60,8 @@ _EOF_
 
 # Entry point
 main() {
-  cd "${PWD}"
+  SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+  cd "${SCRIPT_DIR}"
 
   check_root
   check_prerequisites
