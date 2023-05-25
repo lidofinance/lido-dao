167a305e202b2bc0a4eb4a0e77ec040be1d762f3
Artyom Veremeenko <arwer13@gmail.com>
2023-05-25 12:10:33 +0400

    add extract-artifacts script and regenerate artifacts.json

---
+++ scripts/extract-artifacts.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const path = require('path')
+
+const artifactsFileName = 'artifacts.json'
+const artifactsPath = path.resolve(__dirname, '..', artifactsFileName)
+const deployedFileName = 'deployed-mainnet.json'
+const deployedPath = path.resolve(__dirname, '..', deployedFileName)
+
+
+function isValidContractInfo(info) {
+    let isObject = function(a) {
+      return (!!a) && (a.constructor === Object);
+    }
+    return isObject(info) && ('address' in info) && ('deployTx' in info) && ('contract' in info)
+}
+
+function getArtifactFromContractInfo(info) {
+  let { dir, name, ext} = path.parse(info.contract)
+  return {
+    artifactPath: path.join('artifacts', info.contract, `${name}.json`),
+    sourcePath: info.contract,
+    name: name,
+    address: info.address,
+    txHash: info.deployTx,
+  }
+}
+
+async function extractArtifacts() {
+  const stateFileContent = fs.readFileSync(deployedPath, 'utf8')
+  let state = null
+  try {
+    state = JSON.parse(stateFileContent)
+  } catch (err) {
+    throw new Error(`malformed network state file ${deployedPath}: ${err.message}`)
+  }
+
+  let artifacts = []
+  for (const key in state) {
+    const contractInfo = state[key]
+    if (isValidContractInfo(contractInfo.proxy)) {
+      artifacts.push(getArtifactFromContractInfo(contractInfo.proxy))
+    }
+    if (isValidContractInfo(contractInfo.implementation)) {
+      artifacts.push(getArtifactFromContractInfo(contractInfo.implementation))
+    }
+    if (isValidContractInfo(contractInfo)) {
+      artifacts.push(getArtifactFromContractInfo(contractInfo))
+    }
+  }
+
+  const artifactsJson = JSON.stringify(artifacts, null, '  ')
+  fs.writeFileSync(artifactsPath, artifactsJson + '\n', 'utf8')
+}
+
+extractArtifacts()
+  .then(() => console.log(`Contract artifacts extracted from ${deployedFileName} to ${artifactsFileName}.`))
+  .catch((err) => {
+    console.error(err.stack)
+    process.exit(10)
+  })
