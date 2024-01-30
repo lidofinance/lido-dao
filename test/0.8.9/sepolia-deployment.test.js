617a8f089053f96cc77c9ed01ba227d8fb415c13
VP <vp@lido.fi>
2024-01-30 15:31:16 +0300

    Base test

---
+++ test/0.8.9/sepolia-deployment.test.js
@@ -0,0 +1,27 @@
+const { artifacts, contract, ethers } = require('hardhat')
+
+const { EvmSnapshot } = require('../helpers/blockchain')
+
+const SepoliaDepositAdapter = artifacts.require('SepoliaDepositAdapter')
+
+contract('SepoliaDepositAdapter', ([deployer]) => {
+  let depositAdapter
+  let snapshot
+
+  before('deploy lido with dao', async () => {
+    depositAdapter = await SepoliaDepositAdapter.new(deployer, { from: deployer })
+    const dna = await depositAdapter.dnaDigits()
+    console.log(dna)
+
+    snapshot = new EvmSnapshot(ethers.provider)
+    await snapshot.make()
+  })
+
+  afterEach(async () => {
+    await snapshot.rollback()
+  })
+
+  describe('SepoliaDepositAdapter Logic', () => {
+    it(`state after deployment`, async () => {})
+  })
+})
