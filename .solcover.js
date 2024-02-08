08faf9cd2b74e5d2cc2a20f4a1e106abc4369f68
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2024-02-08 17:35:20 +0100

    fix(ci): add requested changes

--- .solcover.js
+++ .solcover.js
@@ -1,10 +1,16 @@
 module.exports = {
   istanbulReporter: ['html', 'text', 'cobertura'],
-  // REVIEW: Do we need to skip only those? Do we need to skip anything at all?
+  // NOTE: those is are all relative paths to the `contracts` directory and does not support regex
+  //       https://github.com/sc-forks/solidity-coverage/issues/632#issuecomment-1736629543
   skipFiles: [
+    '0.4.24/template',
     '0.4.24/test_helpers',
+    '0.6.11/deposit_contract.sol',
+    '0.6.12/interfaces',
     '0.6.12/mocks',
+    '0.8.9/interfaces',
     '0.8.9/test_helpers',
-    'common/test_helpers'
+    'common/interfaces',
+    'common/test_helpers',
   ]
 }
