f00c20a5aec05356007d56b8cd89c8907dc6cd6d
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2024-02-08 16:09:01 +0100

    build(ci): skip test helper contracts

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,10 @@
 module.exports = {
   istanbulReporter: ['html', 'text', 'cobertura'],
+  // REVIEW: Do we need to skip only those? Do we need to skip anything at all?
+  skipFiles: [
+    '0.4.24/test_helpers',
+    '0.6.12/mocks',
+    '0.8.9/test_helpers',
+    'common/test_helpers'
+  ]
 }
