010f8f803e70c56a01ca7b477def55dc44743b71
SS7896 <154075127+SS7896@users.noreply.github.com>
2024-02-10 10:56:48 +0800

    Update StETH.sol

--- contracts/0.4.24/StETH.sol
+++ contracts/0.4.24/StETH.sol
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2023 Lido <info@lido.fi>
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: GPL-3.0 
 
 /* See contracts/COMPILERS.md */
 pragma solidity 0.4.24;
