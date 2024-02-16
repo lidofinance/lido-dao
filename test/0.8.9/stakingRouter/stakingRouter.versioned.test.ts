fbb1cb27668f3c4238baa167fcdda21d39a7b3af
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2024-02-16 12:13:26 +0100

    test: restore all tests for staking router versioned

--- test/0.8.9/stakingRouter/stakingRouter.versioned.test.ts
+++ test/0.8.9/stakingRouter/stakingRouter.versioned.test.ts
@@ -4,25 +4,51 @@ import { ethers } from "hardhat";
 
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
-import { StakingRouter } from "typechain-types";
+import { OssifiableProxy, StakingRouter, StakingRouter__factory } from "typechain-types";
 
-import { proxify, randomAddress } from "lib";
+import { MAX_UINT256, randomAddress } from "lib";
 
 describe("StakingRouter:Versioned", () => {
-  let owner: HardhatEthersSigner;
+  let admin: HardhatEthersSigner;
+  let user: HardhatEthersSigner;
 
+  let impl: StakingRouter;
+  let proxy: OssifiableProxy;
   let versioned: StakingRouter;
 
-  before(async () => {
-    [owner] = await ethers.getSigners();
-    const depositContract = randomAddress();
-    const impl = await ethers.deployContract("StakingRouter", [depositContract]);
+  const petrifiedVersion = MAX_UINT256;
 
-    [versioned] = await proxify({ impl, admin: owner });
+  before(async () => {
+    [admin, user] = await ethers.getSigners();
+
+    const depositContract = randomAddress();
+
+    impl = await ethers.deployContract("StakingRouter", [depositContract]);
+
+    proxy = await ethers.deployContract("OssifiableProxy", [await impl.getAddress(), admin.address, new Uint8Array()], {
+      from: admin,
+    });
+
+    versioned = StakingRouter__factory.connect(await proxy.getAddress(), user);
   });
 
-  it("Increments version", async () => {
-    await versioned.initialize(randomAddress(), randomAddress(), randomBytes(32));
-    expect(await versioned.getContractVersion()).to.equal(1n);
+  context("constructor", () => {
+    it("Petrifies the implementation", async () => {
+      expect(await impl.getContractVersion()).to.equal(petrifiedVersion);
+    });
+  });
+
+  context("getContractVersion", () => {
+    it("Returns 0 as the initial contract version", async () => {
+      expect(await versioned.getContractVersion()).to.equal(0n);
+    });
+  });
+
+  context("initialize", () => {
+    it("Increments version", async () => {
+      await versioned.initialize(randomAddress(), randomAddress(), randomBytes(32));
+
+      expect(await versioned.getContractVersion()).to.equal(1n);
+    });
   });
 });
