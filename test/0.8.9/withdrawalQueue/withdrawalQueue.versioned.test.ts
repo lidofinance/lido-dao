cd2156f8979239c12988c75918a7a743161eef9a
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2024-02-16 12:54:21 +0100

    test(withdrawal.queue): restore versioned

--- test/0.8.9/withdrawalQueue/withdrawalQueue.versioned.test.ts
+++ test/0.8.9/withdrawalQueue/withdrawalQueue.versioned.test.ts
@@ -3,28 +3,54 @@ import { ethers } from "hardhat";
 
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
 
-import { WithdrawalQueueERC721 } from "typechain-types";
+import { OssifiableProxy, WithdrawalQueueERC721, WithdrawalQueueERC721__factory } from "typechain-types";
 
-import { deployWithdrawalQueue, proxify, randomAddress } from "lib";
+import { deployWithdrawalQueue, MAX_UINT256, randomAddress } from "lib";
 
 describe("WithdrawalQueueERC721:Versioned", () => {
-  let owner: HardhatEthersSigner;
+  let admin: HardhatEthersSigner;
+  let user: HardhatEthersSigner;
+
+  let impl: WithdrawalQueueERC721;
+  let proxy: OssifiableProxy;
   let versioned: WithdrawalQueueERC721;
 
+  const petrifiedVersion = MAX_UINT256;
+
   before(async () => {
-    [owner] = await ethers.getSigners();
+    [admin, user] = await ethers.getSigners();
 
     const deployed = await deployWithdrawalQueue({
-      queueAdmin: owner,
+      queueAdmin: admin,
       doInitialise: false,
     });
 
-    [versioned] = await proxify({ impl: deployed.impl, admin: owner });
+    impl = deployed.impl;
+
+    proxy = await ethers.deployContract("OssifiableProxy", [await impl.getAddress(), admin.address, new Uint8Array()], {
+      from: admin,
+    });
+
+    versioned = WithdrawalQueueERC721__factory.connect(await proxy.getAddress(), user);
   });
 
-  it("getContractVersion", async () => {
-    await versioned.initialize(randomAddress());
+  context("constructor", () => {
+    it("Petrifies the implementation", async () => {
+      expect(await impl.getContractVersion()).to.equal(petrifiedVersion);
+    });
+  });
 
-    expect(await versioned.getContractVersion()).to.equal(1n);
+  context("getContractVersion", () => {
+    it("Returns 0 as the initial contract version", async () => {
+      expect(await versioned.getContractVersion()).to.equal(0n);
+    });
+  });
+
+  context("initialize", () => {
+    it("Increments version", async () => {
+      await versioned.initialize(randomAddress());
+
+      expect(await versioned.getContractVersion()).to.equal(1n);
+    });
   });
 });
