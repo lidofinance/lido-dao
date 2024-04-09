86406a1206033d54fc032122d4f87e4aa2ab323a
Victor P <157478184+vp42ldo@users.noreply.github.com>
2024-04-09 10:33:13 +0300

    Update contracts/0.8.9/sanity_checks/OracleReportSanityChecker.sol
    Co-authored-by: Eugene Mamin <eugene.m@lido.fi>

--- contracts/0.8.9/sanity_checks/OracleReportSanityChecker.sol
+++ contracts/0.8.9/sanity_checks/OracleReportSanityChecker.sol
@@ -433,7 +433,7 @@ contract OracleReportSanityChecker is AccessControlEnumerable {
     /// @notice Applies sanity checks to the accounting params of Lido's oracle report
     /// WARNING. The method has side effects and modifies the state of the contract.
     ///          It's because of negative rebase checks the cummulative sum over the time.
-    ///          It's called from Lido contract that uses old Solidity version and will do a correct
+    ///          It's called from Lido contract that uses the 'old' Solidity version (0.4.24) and will do a correct
     ///          call to this method even it's declared as "view" there.
     /// @param _timeElapsed time elapsed since the previous oracle report
     /// @param _preCLBalance sum of all Lido validators' balances on the Consensus Layer before the
