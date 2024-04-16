c5cbba729fcdf7d8c218696e2d9588d0181c7492
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2024-04-16 11:10:25 +0000

    docs: remove Packed64x4 origin comment, looks like it is a copy-paste from Math256.sol

--- contracts/0.4.24/lib/Packed64x4.sol
+++ contracts/0.4.24/lib/Packed64x4.sol
@@ -1,8 +1,6 @@
 // SPDX-FileCopyrightText: 2023 Lido <info@lido.fi>
 // SPDX-License-Identifier: MIT
 
-// Copied from: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/0457042d93d9dfd760dbaa06a4d2f1216fdbe297/contracts/utils/math/Math.sol
-
 // See contracts/COMPILERS.md
 // solhint-disable-next-line
 pragma solidity ^0.4.24;
