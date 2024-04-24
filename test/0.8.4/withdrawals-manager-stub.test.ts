aa1ffa06a7ac00b8cbc1a4c30b307368bb5bd747
Azat Serikov <azatsdev@gmail.com>
2024-04-24 14:41:57 +0500

    test(stub): reverts always

---
+++ test/0.8.4/withdrawals-manager-stub.test.ts
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+
+import { WithdrawalsManagerStub, WithdrawalsManagerStub__factory } from "typechain-types";
+
+import { ether } from "lib";
+
+describe("WithdrawalsManagerStub", () => {
+  let deployer: HardhatEthersSigner;
+  let sender: HardhatEthersSigner;
+
+  let stub: WithdrawalsManagerStub;
+
+  beforeEach(async () => {
+    [deployer, sender] = await ethers.getSigners();
+
+    stub = await new WithdrawalsManagerStub__factory(deployer).deploy();
+  });
+
+  context("receive", () => {
+    it("Reverts", async () => {
+      await expect(
+        sender.sendTransaction({
+          value: ether("1"),
+          to: stub,
+        }),
+      ).to.be.revertedWith("not supported");
+    });
+  });
+});
