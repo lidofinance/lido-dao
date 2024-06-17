a7f6dcc98ebb77d1a2a944d6e1bc7d5f7b89fbc1
Eugene M <eugene.m@lido.fi>
2024-06-17 10:10:20 +0300

    fix: add initialize scaffold

--- test/0.4.24/nor/node-operators-registry.test.ts
+++ test/0.4.24/nor/node-operators-registry.test.ts
@@ -1,24 +1,89 @@
-//import { expect } from "chai";
-//import { ZeroAddress } from "ethers";
-//import { ethers } from "hardhat";
+import { expect } from "chai";
+import { encodeBytes32String, MaxUint256, ZeroAddress } from "ethers";
+import { ethers } from "hardhat";
 
-//import {
-//    NodeOperatorsRegistryMock,
-//    NodeOperatorsRegistryMock__factory,
-//} from "typechain-types";
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
 
-import { Snapshot } from "lib";
+import { Lido__factory, LidoLocator, NodeOperatorsRegistry, NodeOperatorsRegistry__factory } from "typechain-types";
+
+import { dummyLocator, proxify, Snapshot } from "lib";
 
 describe("NodeOperatorsRegistry", () => {
+  let deployer: HardhatEthersSigner;
+
+  let nor: NodeOperatorsRegistry;
+
   let originalState: string;
 
-  before(async () => {});
+  before(async () => {
+    [deployer] = await ethers.getSigners();
+    const factory = new NodeOperatorsRegistry__factory(deployer);
+    const impl = await factory.deploy();
+
+    expect(await impl.getInitializationBlock()).to.equal(MaxUint256);
+    [nor] = await proxify({ impl, admin: deployer });
+  });
 
   beforeEach(async () => (originalState = await Snapshot.take()));
 
   afterEach(async () => await Snapshot.restore(originalState));
 
-  context("initialize", () => {});
+  context("initialize", () => {
+    //const contractVersion = 2n;
+    const moduleType = encodeBytes32String("curated-onchain-v1");
+
+    let lidoAddress: string;
+    let burnerAddress: string;
+    let locator: LidoLocator;
+
+    beforeEach(async () => {
+      locator = await dummyLocator();
+
+      lidoAddress = await locator.lido();
+      burnerAddress = await locator.burner();
+    });
+
+    it("Reverts if Locator is zero address", async () => {
+      await expect(nor.initialize(ZeroAddress, moduleType, 43200n)).to.be.reverted;
+    });
+
+    it("Reverts if stuck penalty delay exceeds MAX_STUCK_PENALTY_DELAY", async () => {
+      const MAX_STUCK_PENALTY_DELAY = await nor.MAX_STUCK_PENALTY_DELAY();
+      await expect(nor.initialize(locator, "curated-onchain-v1", MAX_STUCK_PENALTY_DELAY + 1n));
+    });
+
+    it("Reverts if already initialized", async () => {
+      const MAX_STUCK_PENALTY_DELAY = await nor.MAX_STUCK_PENALTY_DELAY();
+      await nor.initialize(locator, encodeBytes32String("curated-onchain-v1"), MAX_STUCK_PENALTY_DELAY);
+
+      await expect(nor.initialize(locator, moduleType, MAX_STUCK_PENALTY_DELAY)).to.be.revertedWith(
+        "INIT_ALREADY_INITIALIZED",
+      );
+    });
+
+    it("Makes the contract initialized to v2", async () => {
+      const latestBlock = BigInt(await time.latestBlock());
+      const lido = Lido__factory.connect(lidoAddress);
+
+      await expect(nor.initialize(locator, moduleType, 86400n))
+        .to.emit(nor, "ContractVersionSet")
+        .withArgs(2n)
+        .and.to.emit(nor, "StuckPenaltyDelayChanged")
+        .withArgs(86400n)
+        .and.to.emit(nor, "LocatorContractSet")
+        .withArgs(await locator.getAddress())
+        .and.to.emit(nor, "StakingModuleTypeSet")
+        .withArgs("curated-onchain-v1");
+
+      expect(await nor.getLocator()).to.equal(await locator.getAddress());
+      expect(await nor.getInitializationBlock()).to.equal(latestBlock + 1n);
+      expect(await lido.allowance(await nor.getAddress(), burnerAddress)).to.equal(MaxUint256);
+      expect(await nor.getStuckPenaltyDelay()).to.equal(86400n);
+      expect(await nor.getContractVersion()).to.equal(2n);
+      expect(await nor.getType()).to.equal(moduleType);
+    });
+  });
 
   context("finalizeUpgrade_v2", () => {});
 
