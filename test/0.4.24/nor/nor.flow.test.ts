b8b77da98c90c60c34a34c9bdc37ed613b7305e2
Eugene M <eugene.m@lido.fi>
2024-06-21 00:48:28 +0300

    feat: more tests

--- test/0.4.24/nor/nor.flow.test.ts
+++ test/0.4.24/nor/nor.flow.test.ts
@@ -91,38 +91,30 @@ describe("NodeOperatorsRegistry", () => {
   afterEach(async () => await Snapshot.restore(originalState));
 
   context("addNodeOperator", () => {
-    beforeEach(async () => {
-
-    });
+    beforeEach(async () => {});
 
     it("Reverts if invalid name", async () => {
-      await expect(nor.addNodeOperator("", certainAddress("reward-address-0"))).to.be.revertedWith(
-        "WRONG_NAME_LENGTH"
-      );
+      await expect(nor.addNodeOperator("", certainAddress("reward-address-0"))).to.be.revertedWith("WRONG_NAME_LENGTH");
 
       const maxLength = await nor.MAX_NODE_OPERATOR_NAME_LENGTH();
 
       const longName = "x".repeat(Number(maxLength + 1n));
       await expect(nor.addNodeOperator(longName, certainAddress("reward-address-0"))).to.be.revertedWith(
-        "WRONG_NAME_LENGTH"
+        "WRONG_NAME_LENGTH",
       );
-    })
+    });
 
     it("Reverts if invalid reward address", async () => {
-      await expect(nor.addNodeOperator("abcdef", ZeroAddress)).to.be.revertedWith(
-        "ZERO_ADDRESS"
-      );
+      await expect(nor.addNodeOperator("abcdef", ZeroAddress)).to.be.revertedWith("ZERO_ADDRESS");
 
-      await expect(nor.addNodeOperator("abcdef", lido)).to.be.revertedWith(
-        "LIDO_REWARD_ADDRESS"
-      );
-    })
+      await expect(nor.addNodeOperator("abcdef", lido)).to.be.revertedWith("LIDO_REWARD_ADDRESS");
+    });
 
     it("Reverts if has no MANAGE_NODE_OPERATOR_ROLE assigned", async () => {
       await expect(nor.addNodeOperator("abcdef", certainAddress("reward-address-0"))).to.be.revertedWith(
-        "APP_AUTH_FAILED"
+        "APP_AUTH_FAILED",
       );
-    })
+    });
 
     it("Reverts if MAX_NODE_OPERATORS_COUNT exceeded", async () => {
       const maxNodeOperators = await nor.MAX_NODE_OPERATORS_COUNT();
@@ -133,11 +125,10 @@ describe("NodeOperatorsRegistry", () => {
       }
       await Promise.all(promises);
 
-      await expect(nor.connect(nodeOperatorsManager).addNodeOperator(
-        "abcdef",
-        certainAddress("reward-address-0")
-      )).to.be.revertedWith("MAX_OPERATORS_COUNT_EXCEEDED");
-    })
+      await expect(
+        nor.connect(nodeOperatorsManager).addNodeOperator("abcdef", certainAddress("reward-address-0")),
+      ).to.be.revertedWith("MAX_OPERATORS_COUNT_EXCEEDED");
+    });
 
     it("Adds a new node operator", async () => {
       for (let i = 0n; i < 10; ++i) {
@@ -147,7 +138,7 @@ describe("NodeOperatorsRegistry", () => {
 
         await expect(nor.connect(nodeOperatorsManager).addNodeOperator(name, rewardAddress))
           .to.emit(nor, "NodeOperatorAdded")
-          .withArgs(id, name, rewardAddress, 0n)
+          .withArgs(id, name, rewardAddress, 0n);
 
         expect(await nor.getNodeOperatorsCount()).to.equal(id + 1n);
         const nodeOperator = await nor.getNodeOperator(id, true);
@@ -160,7 +151,7 @@ describe("NodeOperatorsRegistry", () => {
         expect(nodeOperator.totalAddedValidators).to.equal(0n);
         expect(nodeOperator.totalDepositedValidators).to.equal(0n);
       }
-    })
+    });
   });
 
   context("activateNodeOperator", () => {
@@ -170,23 +161,23 @@ describe("NodeOperatorsRegistry", () => {
       expect(await nor.getNodeOperatorIsActive(0n)).to.be.true;
       await nor.connect(nodeOperatorsManager).deactivateNodeOperator(0n);
       expect(await nor.getNodeOperatorIsActive(0n)).to.be.false;
-    })
+    });
 
     it("Reverts if no such an operator exists", async () => {
       await expect(nor.activateNodeOperator(1n)).to.be.revertedWith("OUT_OF_RANGE");
-    })
+    });
 
     it("Reverts if has no MANAGE_NODE_OPERATOR_ROLE assigned", async () => {
       await expect(nor.activateNodeOperator(0n)).to.be.revertedWith("APP_AUTH_FAILED");
-    })
+    });
 
     it("Reverts if already active", async () => {
       await nor.connect(nodeOperatorsManager).addNodeOperator("second", randomAddress());
 
       await expect(nor.connect(nodeOperatorsManager).activateNodeOperator(1n)).to.be.revertedWith(
-        "WRONG_OPERATOR_ACTIVE_STATE"
+        "WRONG_OPERATOR_ACTIVE_STATE",
       );
-    })
+    });
 
     it("Activates an inactive node operator", async () => {
       await expect(nor.connect(nodeOperatorsManager).activateNodeOperator(0n))
@@ -195,24 +186,127 @@ describe("NodeOperatorsRegistry", () => {
         .to.emit(nor, "KeysOpIndexSet")
         .withArgs(2n)
         .to.emit(nor, "NonceChanged")
-        .withArgs(2n)
+        .withArgs(2n);
 
-        const nodeOperator = await nor.getNodeOperator(0n, true);
-        expect(nodeOperator.active).to.be.true;
-        expect(await nor.getNodeOperatorIsActive(0n)).to.be.true;
-    })
+      const nodeOperator = await nor.getNodeOperator(0n, true);
+      expect(nodeOperator.active).to.be.true;
+      expect(await nor.getNodeOperatorIsActive(0n)).to.be.true;
+    });
   });
 
   context("deactivateNodeOperator", () => {
     beforeEach(async () => {
       await nor.connect(nodeOperatorsManager).addNodeOperator("abcdef", randomAddress());
       expect(await nor.getNodeOperatorIsActive(0n)).to.be.true;
-    })
+    });
+
+    it("Reverts if no such an operator exists", async () => {
+      await expect(nor.deactivateNodeOperator(1n)).to.be.revertedWith("OUT_OF_RANGE");
+    });
+
+    it("Reverts if has no MANAGE_NODE_OPERATOR_ROLE assigned", async () => {
+      await expect(nor.deactivateNodeOperator(0n)).to.be.revertedWith("APP_AUTH_FAILED");
+    });
+
+    it("Reverts if already inactive", async () => {
+      await nor.connect(nodeOperatorsManager).addNodeOperator("second", randomAddress());
+      await nor.connect(nodeOperatorsManager).deactivateNodeOperator(1n);
+
+      await expect(nor.connect(nodeOperatorsManager).deactivateNodeOperator(1n)).to.be.revertedWith(
+        "WRONG_OPERATOR_ACTIVE_STATE",
+      );
+    });
+
+    it("Deactivates an active node operator", async () => {
+      await expect(nor.connect(nodeOperatorsManager).deactivateNodeOperator(0n))
+        .to.emit(nor, "NodeOperatorActiveSet")
+        .withArgs(0n, false)
+        .to.emit(nor, "KeysOpIndexSet")
+        .withArgs(1n)
+        .to.emit(nor, "NonceChanged")
+        .withArgs(1n);
+
+      const nodeOperator = await nor.getNodeOperator(0n, true);
+      expect(nodeOperator.active).to.be.false;
+      expect(await nor.getNodeOperatorIsActive(0n)).to.be.false;
+    });
   });
 
-  context("setNodeOperatorName", () => {});
+  context("setNodeOperatorName", () => {
+    beforeEach(async () => {
+      await nor.connect(nodeOperatorsManager).addNodeOperator("abcdef", randomAddress());
+      expect(await nor.getNodeOperatorIsActive(0n)).to.be.true;
+    });
 
-  context("setNodeOperatorRewardAddress", () => {});
+    it("Reverts if the invalid name", async () => {
+      await expect(nor.setNodeOperatorName(0n, "")).to.be.revertedWith("WRONG_NAME_LENGTH");
+
+      const maxLength = await nor.MAX_NODE_OPERATOR_NAME_LENGTH();
+
+      const longName = "x".repeat(Number(maxLength + 1n));
+      await expect(nor.setNodeOperatorName(0n, longName)).to.be.revertedWith("WRONG_NAME_LENGTH");
+    });
+
+    it("Reverts if no such an operator exists", async () => {
+      await expect(nor.setNodeOperatorName(1n, "node-operator-0")).to.be.revertedWith("OUT_OF_RANGE");
+    });
+
+    it("Reverts if has no MANAGE_NODE_OPERATOR_ROLE assigned", async () => {
+      await expect(nor.setNodeOperatorName(0n, "node-operator-0")).to.be.revertedWith("APP_AUTH_FAILED");
+    });
+
+    it("Reverts if already has the same name", async () => {
+      await expect(nor.connect(nodeOperatorsManager).setNodeOperatorName(0n, "abcdef")).to.be.revertedWith(
+        "VALUE_IS_THE_SAME",
+      );
+    });
+
+    it("Renames an existing node operator", async () => {
+      await expect(nor.connect(nodeOperatorsManager).setNodeOperatorName(0n, "node-operator-0"))
+        .to.emit(nor, "NodeOperatorNameSet")
+        .withArgs(0n, "node-operator-0");
+
+      const nodeOperator = await nor.getNodeOperator(0n, true);
+      expect(nodeOperator.name).to.be.equal("node-operator-0");
+    });
+  });
+
+  context("setNodeOperatorRewardAddress", () => {
+    beforeEach(async () => {
+      await nor.connect(nodeOperatorsManager).addNodeOperator("abcdef", certainAddress("node-operator-0"));
+      expect(await nor.getNodeOperatorIsActive(0n)).to.be.true;
+    });
+
+    it("Reverts if invalid reward address", async () => {
+      await expect(nor.setNodeOperatorRewardAddress(0n, ZeroAddress)).to.be.revertedWith("ZERO_ADDRESS");
+
+      await expect(nor.setNodeOperatorRewardAddress(0n, lido)).to.be.revertedWith("LIDO_REWARD_ADDRESS");
+    });
+
+    it("Reverts if no such an operator exists", async () => {
+      await expect(nor.setNodeOperatorRewardAddress(1n, randomAddress())).to.be.revertedWith("OUT_OF_RANGE");
+    });
+
+    it("Reverts if has no MANAGE_NODE_OPERATOR_ROLE assigned", async () => {
+      await expect(nor.setNodeOperatorRewardAddress(0n, randomAddress())).to.be.revertedWith("APP_AUTH_FAILED");
+    });
+
+    it("Reverts if already has the same address", async () => {
+      await expect(
+        nor.connect(nodeOperatorsManager).setNodeOperatorRewardAddress(0n, certainAddress("node-operator-0")),
+      ).to.be.revertedWith("VALUE_IS_THE_SAME");
+    });
+
+    it("Sets a reward address for an existing node operator", async () => {
+      const addr = certainAddress("new-address");
+      await expect(nor.connect(nodeOperatorsManager).setNodeOperatorRewardAddress(0n, addr))
+        .to.emit(nor, "NodeOperatorRewardAddressSet")
+        .withArgs(0n, addr);
+
+      const nodeOperator = await nor.getNodeOperator(0n, true);
+      expect(nodeOperator.rewardAddress).to.be.equal(addr);
+    });
+  });
 
   context("setNodeOperatorStakingLimit", () => {});
 
