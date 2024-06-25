8fa0b5834a39e7bbe4e6f14f53efcad18eb510d2
Anna Mukharram <amuhar3@gmail.com>
2024-06-26 00:25:35 +0400

    fix: get rid of env for contracts parameters

--- scripts/staking-router-v2/sr-v2-deploy.ts
+++ scripts/staking-router-v2/sr-v2-deploy.ts
@@ -26,13 +26,20 @@ function getEnvVariable(name: string, defaultValue?: string) {
 }
 
 async function main() {
-  // parameters from env variables
-  const PAUSE_INTENT_VALIDITY_PERIOD_BLOCKS = parseInt(getEnvVariable("PAUSE_INTENT_VALIDITY_PERIOD_BLOCKS"));
-  const MAX_OPERATORS_PER_UNVETTING = parseInt(getEnvVariable("MAX_OPERATORS_PER_UNVETTING"));
-  const SECONDS_PER_SLOT = parseInt(getEnvVariable("SECONDS_PER_SLOT"));
-  const GENESIS_TIME = parseInt(getEnvVariable("GENESIS_TIME"));
+  // DSM args
+  const PAUSE_INTENT_VALIDITY_PERIOD_BLOCKS = 7200;
+  const MAX_OPERATORS_PER_UNVETTING = 20;
+
+  // Accounting Oracle args
+  const SECONDS_PER_SLOT = 12;
+  const GENESIS_TIME = 1606824023;
+
+  // Oracle report sanity checker
+  // 43200 check value
+  const LIMITS = [9000, 500, 1000, 50, 600, 4, 59, 7680, 750000, 43200];
+  const MANAGERS_ROSTER = [[], [], [], [], [], [], [], [], [], [], []];
+
   const deployer = ethers.getAddress(getEnvVariable("DEPLOYER"));
-  const APPEARED_VALIDATORS_PER_DAY_LIMIT = parseInt(getEnvVariable("APPEARED_VALIDATORS_PER_DAY_LIMIT"));
   const chainId = (await ethers.provider.getNetwork()).chainId;
 
   const balance = await ethers.provider.getBalance(deployer);
@@ -42,14 +49,25 @@ async function main() {
   state[Sk.scratchDeployGasUsed] = 0n.toString();
   persistNetworkState(state);
 
-  const maxPositiveTokenRebaseManagers: string[] = [];
-  const currentLimits = state[Sk.oracleReportSanityChecker].constructorArgs[2];
-  const managersRoster = state[Sk.oracleReportSanityChecker].constructorArgs[3];
-  const LIMITS_LIST = [...currentLimits, APPEARED_VALIDATORS_PER_DAY_LIMIT];
-  const MANAGERS_ROSTER = [...managersRoster, maxPositiveTokenRebaseManagers];
+  // holesky
+  // "0x711B5fCfeD5A30CA78e0CAC321B060dE9D6f8979",
+  // "0xDAaE8C017f1E2a9bEC6111d288f9ebB165e0E163",
+  // "0x31fa51343297FFce0CC1E67a50B2D3428057D1b1",
+  // "0x43464Fe06c18848a2E2e913194D64c1970f4326a",
+  // "0x79A132BE0c25cED09e745629D47cf05e531bb2bb",
+  // "0x0bf1B3d1e6f78b12f26204348ABfCA9310259FfA",
+  // "0xf060ab3d5dCfdC6a0DFd5ca0645ac569b8f105CA"
+  // quorum = 3
 
-  const guardians = state[Sk.depositSecurityModule].guardians.addresses;
-  const quorum = state[Sk.depositSecurityModule].guardians.quorum;
+  const guardians = [
+    "0x5fd0dDbC3351d009eb3f88DE7Cd081a614C519F1",
+    "0x7912Fa976BcDe9c2cf728e213e892AD7588E6AaF",
+    "0x14D5d5B71E048d2D75a39FfC5B407e3a3AB6F314",
+    "0xf82D88217C249297C6037BA77CE34b3d8a90ab43",
+    "0xa56b128Ea2Ea237052b0fA2a96a387C0E43157d8",
+    "0xd4EF84b638B334699bcf5AF4B0410B8CCD71943f",
+  ];
+  const quorum = 4;
 
   // Read contracts addresses from config
   const DEPOSIT_CONTRACT_ADDRESS = state[Sk.chainSpec].depositContractAddress;
@@ -123,7 +141,7 @@ async function main() {
 
   log(`AO implementation address: ${accountingOracleAddress}`);
 
-  const oracleReportSanityCheckerArgs = [LOCATOR, SC_ADMIN, LIMITS_LIST, MANAGERS_ROSTER];
+  const oracleReportSanityCheckerArgs = [LOCATOR, SC_ADMIN, LIMITS, MANAGERS_ROSTER];
 
   const oracleReportSanityCheckerAddress = (
     await deployWithoutProxy(
