6177bf0e1aa5123b68a5632c487999a50f3ed196
Eddort <webisforce@gmail.com>
2024-07-08 23:00:50 +0200

    fix: types in deploy hash consensus

--- test/deploy/hashConsensus.ts
+++ test/deploy/hashConsensus.ts
@@ -1,6 +1,6 @@
 import { ethers } from "hardhat";
 
-import { MockReportProcessor } from "typechain-types";
+import { AccountingOracleTimeTravellable, MockReportProcessor } from "typechain-types";
 
 import {
   CONSENSUS_VERSION,
@@ -15,7 +15,7 @@ import {
 export async function deployHashConsensus(
   admin: string,
   {
-    reportProcessor = null as MockReportProcessor | null,
+    reportProcessor = null as MockReportProcessor | AccountingOracleTimeTravellable | null,
     slotsPerEpoch = SLOTS_PER_EPOCH,
     secondsPerSlot = SECONDS_PER_SLOT,
     genesisTime = GENESIS_TIME,
