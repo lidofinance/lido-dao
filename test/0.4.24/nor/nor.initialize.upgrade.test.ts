85665da29c7a3a5f8205b6f0567ccb31997e4554
Alex <webisforce@gmail.com>
2024-07-19 13:45:58 +0400

    Update test/0.4.24/nor/nor.initialize.upgrade.test.ts
    Co-authored-by: avsetsin <mail@sjors.ru>

--- test/0.4.24/nor/nor.initialize.upgrade.test.ts
+++ test/0.4.24/nor/nor.initialize.upgrade.test.ts
@@ -166,7 +166,7 @@ describe("NodeOperatorsRegistry:initialize-and-upgrade", () => {
       );
     });
 
-    it("Reverts if already initialized with v2", async () => {
+    it("Reverts if already initialized", async () => {
       const MAX_STUCK_PENALTY_DELAY = await nor.MAX_STUCK_PENALTY_DELAY();
       await nor.initialize(locator, encodeBytes32String("curated-onchain-v1"), MAX_STUCK_PENALTY_DELAY);
 
