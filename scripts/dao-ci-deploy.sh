b820ee55e329b8a645f286d6f6bde152dc66cc09
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2024-08-28 16:30:48 +0100

    fix: fix scratch deploy env

--- scripts/dao-ci-deploy.sh
+++ scripts/dao-ci-deploy.sh
@@ -5,6 +5,11 @@ set -o pipefail
 export NETWORK=local
 export RPC_URL=${RPC_URL:="http://127.0.0.1:8555"}  # if defined use the value set to default otherwise
 
+export GENESIS_TIME=1639659600  # just some time
+export DEPLOYER=0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266  # first acc of default mnemonic "test test ..."
+export GAS_PRIORITY_FEE=1
+export GAS_MAX_FEE=100
+
 export NETWORK_STATE_FILE="deployed-${NETWORK}.json"
 export NETWORK_STATE_DEFAULTS_FILE="scripts/scratch/deployed-testnet-defaults.json"
 
