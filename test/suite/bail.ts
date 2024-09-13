ff369b04fe5093ca3e711333f89b1883ff4073fd
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2024-09-13 14:40:34 +0100

    chore: add comment

--- test/suite/bail.ts
+++ test/suite/bail.ts
@@ -1,3 +1,7 @@
+/**
+ * Bail on failure if any test in the current suite has failed.
+ * Can be used as a `beforeEach` hook to skip tests that are dependent on the previous tests passing.
+ */
 export function bailOnFailure(this: Mocha.Context) {
   if (this.currentTest?.parent?.tests.some((t) => t.state === "failed")) {
     this.skip();
